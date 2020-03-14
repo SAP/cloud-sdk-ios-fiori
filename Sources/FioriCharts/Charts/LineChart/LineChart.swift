f9b7abd96817662903924dd4f8f7d5c526604b67
Sheng Xu <sheng.xu01@samp.com>
2020-03-13 17:34:40 -0700

    rename folder names of api to model and MicroChart to Chart

---
+++ Sources/FioriCharts/Charts/LineChart/LineChart.swift
@@ -0,0 +1,20 @@
+//
+//  LineChart.swift
+//  FioriCharts
+//
+//  Created by Xu, Sheng on 3/13/20.
+//
+
+import SwiftUI
+
+struct LineChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LineChart_Previews: PreviewProvider {
+    static var previews: some View {
+        LineChart()
+    }
+}
