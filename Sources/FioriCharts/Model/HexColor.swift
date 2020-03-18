5a7a95e643aeae15252e4466280181c5b0ab4c6a
Sheng Xu <sheng.xu01@sap.com>
2020-03-18 13:01:12 -0700

    add a public init for HexColor

--- Sources/FioriCharts/Model/HexColor.swift
+++ Sources/FioriCharts/Model/HexColor.swift
@@ -11,6 +11,25 @@ import SwiftUI
 public struct HexColor: Hashable {
     var colors: [ColorScheme: String]
     
+    /*
+     Constructs a HexColor from a lightColor and darkColor
+     
+     - parameter:
+        - lightColor: a hex color (RGB or RGBA) for .light mode, default is "000000"
+        - darkColor: a hex color (RGB or RGBA) for .dark mode, default is "FFFFFF"
+     */
+    public init(lightColor: String? = nil, darkColor: String? = nil) {
+        self.colors = [.light: "000000", .dark: "FFFFFF"]
+        
+        if let color = lightColor {
+            colors[.light] = color
+        }
+        
+        if let color = darkColor {
+            colors[.dark] = color
+        }
+    }
+    
     public func color(_ colorScheme: ColorScheme = .light) -> Color {
         if let hex = colors[colorScheme] {
             return Color(hex: hex)
