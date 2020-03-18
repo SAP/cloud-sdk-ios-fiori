c3c8a9168e28ca48159dd5be363a741a66a97c8f
Bill Zhou <bill.zhou01@sap.com>
2020-03-17 23:46:05 -0700

    make library type automatic

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "FioriSwiftUI",
-            type: .dynamic,
             targets: ["FioriSwiftUI"]),
     ],
     dependencies: [
