79e43da68eeee2bd10dff8224588f73626e38d4d
Stan Stadelman <sstadelman@gmail.com>
2020-03-18 15:28:41 -0700

    added some sample data

---
+++ Sources/FioriIntegrationCards/Utils/DecodableUtils/HavingGroups.swift
@@ -0,0 +1,13 @@
+//
+//  HavingGroups.swift
+//  FioriSwiftUI
+//
+//  Created by Stan Stadelman on 3/18/20.
+//
+
+import Foundation
+
+
+struct HavingGroups<Model: Decodable>: Decodable {
+    let groups: Model
+}
