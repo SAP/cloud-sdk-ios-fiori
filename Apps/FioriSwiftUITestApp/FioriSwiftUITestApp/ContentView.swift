aaee8f788d4c81a418a0f993d1093ad301d78e44
Xiao Ma <xiao.ma01@sap.com>
2020-03-19 13:27:15 -0700

    Add test app for FioriSwiftUI

---
+++ Apps/FioriSwiftUITestApp/FioriSwiftUITestApp/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  FioriSwiftUITestApp
+//
+//  Created by Ma, Xiao on 3/19/20.
+//  Copyright © 2020 Ma, Xiao. All rights reserved.
+//
+
+import SwiftUI
+import FioriSwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        DimensionSelector(titles: [
+            "1D",
+            "1W",
+            "1M",
+            "1Q",
+            "YTD",
+            "1Y",
+            "5Y"
+        ])
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
