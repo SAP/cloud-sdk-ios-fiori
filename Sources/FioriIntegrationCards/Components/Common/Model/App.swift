7195f2691e36965a667e43948ca56a0dd1fbec94
Stan Stadelman <sstadelman@gmail.com>
2020-03-21 23:12:58 -0700

    add support for parsing DataSources

--- Sources/FioriIntegrationCards/Components/Common/Model/App.swift
+++ Sources/FioriIntegrationCards/Components/Common/Model/App.swift
@@ -14,4 +14,23 @@ public struct App: Codable, Identifiable, Hashable {
     public let type: String
     public let title: String?
     public let subTitle: String?
+    public let dataSources: [String: DataSource]
+    
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case type
+        case title
+        case subTitle
+        case dataSources
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(String.self, forKey: .id)
+        type = try container.decode(String.self, forKey: .type)
+        title = try container.decodeIfPresent(String.self, forKey: .title)
+        subTitle = try container.decodeIfPresent(String.self, forKey: .subTitle)
+        dataSources = try container.decodeIfPresent([String: DataSource].self, forKey: .dataSources) ?? [:]
+        
+    }
 }
