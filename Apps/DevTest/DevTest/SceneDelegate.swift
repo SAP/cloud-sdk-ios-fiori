cec8da248b67349aa08358608f603bc9c213d138
Stan Stadelman <sstadelman@gmail.com>
2020-04-02 22:26:37 -0700

    added all cases to integration cards collection view

--- Apps/DevTest/DevTest/SceneDelegate.swift
+++ Apps/DevTest/DevTest/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         let oneMany = ["table", "list", "timeline"]
         let manyMany = ["LowCode", "object", "analytical"]
-        let cards: [String] = [/*"LowCode", "timeline", "table", "list" , "object",*/ "analytical"]
+        let cards: [String] = ["LowCode", "timeline", "table", "list" , "object", "analytical"]
         
         let contentView = ContentView(cards: cards)
         
