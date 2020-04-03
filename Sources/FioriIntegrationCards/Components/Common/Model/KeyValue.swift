568ac185c7929fd2f1ba44ad0c5af6d5e87ee72d
Stan Stadelman <sstadelman@gmail.com>
2020-04-02 22:14:27 -0700

    refactored analytical card to match others, introduced prototype collectionView

---
+++ Sources/FioriIntegrationCards/Components/Common/Model/KeyValue.swift
@@ -0,0 +1,21 @@
+//
+//  KeyValue.swift
+//  AnyCodable
+//
+//  Created by Stan Stadelman on 3/30/20.
+//
+
+import Foundation
+
+public struct KeyValue: Codable, Hashable {
+    let label: String?
+    let value: String
+}
+
+extension KeyValue : Placeholding {
+    public func replacingPlaceholders(withValuesIn object: Any) -> KeyValue {
+        let _label = label?.replacingPlaceholders(withValuesIn: object)
+        let _value = value.replacingPlaceholders(withValuesIn: object)
+        return KeyValue(label: _label, value: _value)
+    }
+}
