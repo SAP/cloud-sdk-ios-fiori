b6fce361e27e86a97572243a991c2bd00bda5693
Xiao Ma <xiao.ma01@sap.com>
2020-04-21 12:12:39 -0700

    Add theming related files

---
+++ Sources/FioriSwiftUI/DimensionSelector/ControlState.swift
@@ -0,0 +1,12 @@
+//
+//  ControlState.swift
+//  FioriSwiftUI
+//
+//  Created by Ma, Xiao on 4/21/20.
+//
+import SwiftUI
+
+struct ControlStateColor {
+    var selected: Color
+    var normal: Color
+}
