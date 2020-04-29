13a4963cf19b7279a68f2ef49a1fb43cdc5f6db4
Stan Stadelman <sstadelman@gmail.com>
2020-04-29 14:51:16 -0700

    restructured integrationcard dependency

--- Package.swift
+++ Package.swift
@@ -7,15 +7,14 @@ let package = Package(
     name: "FioriSwiftUI",
     platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "FioriSwiftUI",
             type: .dynamic,
             targets: ["FioriSwiftUI"]),
         .library(
-        name: "Cards",
-        type: .dynamic,
-        targets: ["FioriIntegrationCards"]),
+            name: "FioriIntegrationCards",
+            type: .dynamic,
+            targets: ["FioriIntegrationCards"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,15 +22,11 @@ let package = Package(
         .package(url: "https://github.com/Flight-School/AnyCodable.git", from: "0.2.3"),
         .package(url: "https://github.com/sstadelman/observable-array.git", .branch("master")),
         .package(url: "https://github.com/marmelroy/Zip.git", .branch("master"))
-//        .package(url: "https://github.com/icanzilb/TimelaneCombine", from: "1.0.0")
-//        .package(url: "https://github.com/marksands/BetterCodable.git", .branch("master"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FioriSwiftUI",
-            dependencies: ["FioriIntegrationCards", "FioriCharts"]),
+            dependencies: ["FioriCharts"]),
         .target(
             name: "FioriIntegrationCards",
             dependencies: ["AnyCodable", "TinyNetworking", "ObservableArray", "FioriCharts", "Zip"]),
