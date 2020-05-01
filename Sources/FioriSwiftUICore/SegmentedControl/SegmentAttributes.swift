30587ac07315f16970d3e21976bcbfc39273381c
Xiao Ma <xiao.ma01@sap.com>
2020-05-01 12:15:01 -0700

    modified model and add fixedWidth and fixedSize support

--- Sources/FioriSwiftUICore/SegmentedControl/SegmentAttributes.swift
+++ Sources/FioriSwiftUICore/SegmentedControl/SegmentAttributes.swift
@@ -11,16 +11,20 @@ import SwiftUI
 public struct SegmentAttributes {
     public var fontColor: Color?
     public var fontSize: CGFloat?
-    public var font: Font?
+    public var font: UIFont?
     public var borderColor: Color?
     
     public init(fontColor: Color? = nil,
                 fontSize: CGFloat? = nil,
-                font: Font? = nil,
+                font: UIFont? = nil,
                 borderColor: Color? = nil) {
         self.fontColor      = fontColor
         self.fontSize       = fontSize
         self.font           = font
         self.borderColor    = borderColor
+    }
+    
+    func getFont() -> Font {
+        return Font(self.font ?? UIFont.preferredFont(forTextStyle: .subheadline))
     }
 }
--- Sources/FioriSwiftUICore/SegmentedControl/SegmentedControl.swift
+++ Sources/FioriSwiftUICore/SegmentedControl/SegmentedControl.swift
@@ -40,7 +40,7 @@ public struct SegmentedControl: View {
             model.titleInset = newValue
         }
     }
-
+    
     public var interItemSpacing: CGFloat {
         get {
             return model.interItemSpacing
@@ -49,7 +49,7 @@ public struct SegmentedControl: View {
             model.interItemSpacing = newValue
         }
     }
-        
+    
     public var selectedIndex: Int? {
         get {
             return model.selectedIndex
@@ -77,6 +77,25 @@ public struct SegmentedControl: View {
         }
     }
     
+    public var fixedSegmentWidth: Bool {
+        get {
+            return model.fixedSegmentWidth
+        }
+        set {
+            model.fixedSegmentWidth = newValue
+            fixedSize = newValue ? getFixedSize(for: self.titles) : nil
+        }
+    }
+    
+    public var fixedSize: CGSize? {
+        get {
+            return model.fixedSize
+        }
+        set {
+            model.fixedSize = newValue
+        }
+    }
+    
     public var contentInset: EdgeInsets {
         get {
             let leadingAndTrailing: CGFloat = horizontalSizeClass == .compact ? 16 : 48
@@ -91,25 +110,27 @@ public struct SegmentedControl: View {
     lazy public private(set) var selectionDidChangePublisher: AnyPublisher<Int?, Never> = {
         self.model.$selectedIndex.eraseToAnyPublisher()
     }()
-        
-    @ObservedObject private var model: Model = Model()
     
+    @ObservedObject private var model: Model = Model()
+        
     public init(segmentTitles: [String],
                 interItemSpacing: CGFloat = 6,
                 titleInsets: EdgeInsets = EdgeInsets.init(top: 8, leading: 8, bottom: 8, trailing: 8),
                 selectedIndex: Int? = nil,
                 contentInset: EdgeInsets? = nil) {
-        self.titles      = segmentTitles
+        
+        self.model.segmentAttributes = [
+            .normal: SegmentAttributes(fontColor: .gray, font: UIFont.preferredFont(forTextStyle: .subheadline), borderColor: .init(red: 0.2, green: 0.2, blue: 0.2)),
+            .selected: SegmentAttributes(fontColor: .blue, font: UIFont.preferredFont(forTextStyle: .subheadline), borderColor: .blue),
+            .disabled: SegmentAttributes(fontColor: .gray, font: UIFont.preferredFont(forTextStyle: .subheadline), borderColor: .init(red: 0.2, green: 0.2, blue: 0.2))
+        ]
+        
+        self.titles             = segmentTitles
         self.titleInsets        = titleInsets
         self.interItemSpacing   = interItemSpacing
         self.model.isEnable     = true
+        self.fixedSegmentWidth  = false
         self.selectedIndex      = selectedIndex
-        
-        self.model.segmentAttributes    = [
-            .normal: SegmentAttributes(fontColor: .gray, borderColor: .init(red: 0.2, green: 0.2, blue: 0.2)),
-            .selected: SegmentAttributes(fontColor: .blue, borderColor: .blue),
-            .disabled: SegmentAttributes(fontColor: .gray, borderColor: .init(red: 0.2, green: 0.2, blue: 0.2))
-        ]
         
         if let _contentInset = contentInset {
             self.contentInset = _contentInset
@@ -120,12 +141,13 @@ public struct SegmentedControl: View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(alignment: .center, spacing: self.model.interItemSpacing) {
                 ForEach(self.model.titles.indices, id: \.self) { index in
-                    Segment(title: self.model.titles[index], isSelected: self.model.selectedIndex == index, isEnable: self.model.isEnable, segmentAttributes: self.model.segmentAttributes, titleInset: self.model.titleInset)
+                    Segment(title: self.model.titles[index], isSelected: self.model.selectedIndex == index, isEnable: self.model.isEnable, segmentAttributes: self.model.segmentAttributes, titleInset: self.model.titleInset, fixedSize: self.fixedSize)
                         .onTapGesture {
                             if self.model.isEnable {
                                 self.selectionDidChange(index: index)
+                                print(self.getFixedSize(for: self.titles))
                             }
-                        }
+                    }
                 }
             }
             .padding([.top, .bottom], 8)
@@ -133,11 +155,40 @@ public struct SegmentedControl: View {
             .padding(.trailing, model.contentInset?.trailing ?? (horizontalSizeClass == .compact ? 16 : 48))
         }
     }
-
+    
     private func selectionDidChange(index: Int?) {
         if selectedIndex != index {
             self.model.selectedIndex = index
         }
+    }
+    
+    private func getFixedSize(for titles: [String]) -> CGSize {
+        var largestWidth: CGFloat = -1
+        var largestHeight: CGFloat = -1
+        
+        let fonts = [segmentAttributes[.normal]?.font,
+                     segmentAttributes[.disabled]?.font,
+                     segmentAttributes[.selected]?.font]
+        
+        for font in fonts {
+            if let _font = font {
+                largestHeight = max(largestHeight, getSizeForString(titles[0], font: _font).height)
+            }
+        }
+        
+        for title in titles {
+            let label = UILabel()
+            label.text = title
+            label.sizeToFit()
+            largestWidth = max(label.frame.width, largestWidth)
+        }
+        
+        return CGSize(width: largestWidth + titleInsets.leading + titleInsets.trailing,
+                      height: largestHeight + titleInsets.top + titleInsets.bottom)
+    }
+    
+    private func getSizeForString(_ string: String, font: UIFont) -> CGSize {
+        return (string as NSString).size(withAttributes: [NSAttributedString.Key.font : font])
     }
 }
 
@@ -149,16 +200,19 @@ extension SegmentedControl {
         var isSelected: Bool
         
         var isEnable: Bool
-                
+        
         var segmentAttributes: [ControlState: SegmentAttributes]
         
         var titleInset: EdgeInsets
-
+        
+        var fixedSize: CGSize?
+        
         var body: some View {
             Text(title)
-                .font(self.isSelected ? segmentAttributes[.selected]?.font : segmentAttributes[.normal]?.font)
+                .font(self.isSelected ? segmentAttributes[.selected]?.getFont(): segmentAttributes[.normal]?.getFont())
                 .padding(titleInset)
                 .foregroundColor(isEnable ? (self.isSelected ? segmentAttributes[.selected]?.fontColor : segmentAttributes[.normal]?.fontColor) : (segmentAttributes[.disabled]?.fontColor))
+                .frame(width: self.fixedSize?.width, height: self.fixedSize?.height)
                 .overlay(ButtonOverlayView(isSelected: self.isSelected, isEnable: self.isEnable, segmentAttributes: segmentAttributes))
         }
     }
@@ -171,6 +225,8 @@ extension SegmentedControl {
         @Published var segmentAttributes: [ControlState: SegmentAttributes]!
         @Published var contentInset: EdgeInsets?
         @Published var isEnable: Bool!
+        @Published var fixedSegmentWidth: Bool!
+        @Published var fixedSize: CGSize?
     }
 }
 
@@ -179,5 +235,4 @@ struct DimensionSelector_Previews: PreviewProvider {
         /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
     }
 }
-
 
