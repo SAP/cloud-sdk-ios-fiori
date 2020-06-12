f52745fe58ea4a79ba01fb7254a4f3938b68a8e8
Marco Eidinger <marco.eidinger@sap.com>
2020-06-12 08:55:46 -0700

    chore: housekeeping
    - add version info for script dependencies in CONTRIBUTING.md
    - improve README
    - provide sample for future script to automate release process
    - remove if push condition in jazzy.yml

---
+++ scripts/generateRelease.sh
@@ -0,0 +1,6 @@
+# Script to be used in future (without dry run) in CI to automate release process with standard-version (https://github.com/conventional-changelog/standard-version)
+# use --release-as option if automatic version bumping is not wanted
+
+npx standard-version --tag-prefix '' --dry-run # --release-as '0.6.0'
+
+# Run `git push --follow-tags origin master` to publish
