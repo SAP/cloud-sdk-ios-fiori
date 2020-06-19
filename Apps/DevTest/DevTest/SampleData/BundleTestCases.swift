63798e27a437812c26c9d4ff2d2d4e447dc30d09
Xiao Ma <xiao.ma01@sap.com>
2020-06-19 12:01:39 -0700

    feat: 🎸 Support fetching data with Request object
    DataFetcher and BaseBaseCard should be configured to execute an
    
    BREAKING CHANGE: 🧨 publisher.
    
    ✅ Closes: #33

--- Apps/DevTest/DevTest/SampleData/BundleTestCases.swift
+++ Apps/DevTest/DevTest/SampleData/BundleTestCases.swift
@@ -8,18 +8,41 @@
 
 import Foundation
 import FioriIntegrationCards
+import Zip
 
 enum BundleTestCases: String, CaseIterable, Identifiable {
-    case HTTPTimelineVarients = "HTTPTimelineVariants.card"
-    case BundleTimelineVarients = "TimelineVariants.card"
+    case HTTPTimelineVarients       = "HTTPTimelineVariants.card"
+    case HTTPObjectVarients         = "HTTPObjectVariants.card"
+    case BundleTimelineVarients     = "TimelineVariants.card"
+    case BundleObjectVarients       = "ObjectVariants.card"
     
     var id: String {
         return rawValue
     }
     
+    func path() -> URL? {
+        
+        let destinationDir = FileManager.default.temporaryDirectory.appendingPathComponent(rawValue, isDirectory: true)
+        if FileManager.default.fileExists(atPath: destinationDir.path) {
+            return destinationDir
+        } else {
+            let sourceFile = Bundle.main.url(forResource: rawValue, withExtension: "zip")!
+            do {
+                try Zip.unzipFile(sourceFile, destination: FileManager.default.temporaryDirectory, overwrite: true, password: nil)
+                return destinationDir
+            } catch {
+                print(error)
+            }
+        }
+        return nil
+    }
+    
     func manifest() -> Manifest? {
+        guard let url = path() else {
+            return nil
+        }
         do {
-            let model = try Manifest.init(with: self.id)
+            let model = try Manifest.init(withCardBundleAt: url)
             return model
         } catch {
             print(error.localizedDescription)
