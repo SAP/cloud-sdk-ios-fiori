8c324df954582ceb339469a66dd5fef57c789be4
Marco Eidinger <marco.eidinger@sap.com>
2020-06-19 14:19:25 -0700

    fix: project ‘FioriSwiftUI’ is damaged and cannot be opened
    occurs when using shell script setupForCollaborators
    with python 2.7 :(
    
    change setupForCollaborators.sh in such a way that it
    attempts to add the run script when python version 3
    is present.
    
    Users with python 2.7 have to manually add run script

--- setupForCollaborators.sh
+++ setupForCollaborators.sh
@@ -9,5 +9,15 @@ bash scripts/installGitHooks.sh
 rm -f -r ./FioriSwiftUI.xcodeproj
 swift package generate-xcodeproj
 
-# add run script to xocde project file
+# add run script to xcode project file (if python 3.7+ is available)
+if ! hash python; then
+    echo "WARNING: no run script was added to Xcode project file to execute SwiftLint check because python version of 3.7+ required"
+    exit 0
+fi
+
+ver=$(python -V 2>&1 | sed 's/.* \([0-9]\).\([0-9]\).*/\1\2/')
+if [ "$ver" -lt "37" ]; then
+    echo "WARNING: no run script was added to Xcode project file to execute SwiftLint check because python version of 3.7+ required"
+    exit 0
+fi
 python scripts/addSwiftLintRunScriptToXcodeProj.py
