7cdc7bdf5322263d0ab8a752e0eab5345e3410bd
Stan Stadelman <sstadelman@gmail.com>
2020-08-25 20:29:19 -0700

    reverted platform bump

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "FioriSwiftUI",
     defaultLocalization: "en",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "FioriSwiftUI",
