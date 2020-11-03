c5e4d1a03b3d919ab640ef1d254bd29eed31b876
Sheng Xu <sheng.xu01@sap.com>
2020-11-03 10:44:39 -0800

    feat: 🎸 small improvements to test app

--- Apps/Examples/Examples/ContentView.swift
+++ Apps/Examples/Examples/ContentView.swift
@@ -30,6 +30,7 @@ struct ContentView: View {
                 }
             }
             .navigationBarTitle("Examples")
+            .navigationViewStyle(StackNavigationViewStyle())
         }
     }
 }
