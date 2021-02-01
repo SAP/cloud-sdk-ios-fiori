5caca1e99ea887fca2178bcd178cbdd811e18e9c
Sheng Xu <sheng.xu01@sap.com>
2021-02-01 15:22:54 -0800

    feat: 🎸 add ShapeStyle to Charts in Examples app

--- Apps/Examples/Examples/ContentView.swift
+++ Apps/Examples/Examples/ContentView.swift
@@ -20,5 +20,11 @@ struct ContentView: View {
             .navigationBarTitle("Examples")
             .navigationViewStyle(StackNavigationViewStyle())
         }
+        .chartSeriesShapeStyle([1: LinearGradient(gradient: Gradient(colors: [.red, .green, .blue]), startPoint: .bottom, endPoint: .top)])
+        .chartCategoryShapeStyle([0: [0: LinearGradient(gradient: Gradient(colors: [.red, .green, .blue]), startPoint: .bottom, endPoint: .top),
+                                      1: Color.yellow,
+                                      2: AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .pink]), center: UnitPoint(x: 0.50, y: 1.00), startAngle: Angle(degrees: 180.00), endAngle: Angle(degrees: 360.00))],
+                                  1: [3: RadialGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .pink]), center: .center, startRadius: 0, endRadius: 50),
+                                      4: ImagePaint(image: Image("flower"))]])
     }
 }
