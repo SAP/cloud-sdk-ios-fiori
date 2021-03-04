f7839de3302f3b7f28b64a1fb0a103ab0bb1171b
Fred Wirjo <fred.wirjo@sap.com>
2021-03-04 09:51:41 -0800

    refactor: 💡 removed unnessary import

--- Apps/Examples/Examples/FioriSwiftUICore/SignatureView/SignatureView.swift
+++ Apps/Examples/Examples/FioriSwiftUICore/SignatureView/SignatureView.swift
@@ -1,26 +1,14 @@
-//
-//  SignatureView.swift
-//  Examples
-//
-//  Created by Wirjo, Fred on 11/4/20.
-//  Copyright © 2020 SAP. All rights reserved.
-//
-
-import SwiftUI
-import FioriCharts
 import FioriSwiftUICore
-import Combine
+import SwiftUI
 
 struct SignatureView_Example: View {
-    
     var signatureView: SignatureView!
     
     init() {
-        signatureView = SignatureView()
+        self.signatureView = SignatureView()
     }
     
     var body: some View {
-        
         VStack(alignment: .center, spacing: 30) {
             signatureView
         }
--- Apps/Examples/Examples/FioriSwiftUICore/SignatureView/SignatureViewInline.swift
+++ Apps/Examples/Examples/FioriSwiftUICore/SignatureView/SignatureViewInline.swift
@@ -1,26 +1,14 @@
-//
-//  SignatureViewInline.swift
-//  Examples
-//
-//  Created by Wirjo, Fred on 11/23/20.
-//  Copyright © 2020 SAP. All rights reserved.
-//
-
-import SwiftUI
-import FioriCharts
 import FioriSwiftUICore
-import Combine
+import SwiftUI
 
 struct SignatureViewInline_Example: View {
-    
     var signatureViewInline: SignatureViewInline!
     
     init() {
-        signatureViewInline = SignatureViewInline()
+        self.signatureViewInline = SignatureViewInline()
     }
     
     var body: some View {
-        
         VStack(alignment: .center, spacing: 30) {
             signatureViewInline
         }
