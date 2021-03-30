de2f9e4f2f0c23e22da7e416f852afb9e30585b1
Fred Wirjo <fred.wirjo@sap.com>
2021-03-30 15:30:09 -0700

    refactor: 💡 remove unused functions

--- Sources/FioriSwiftUICore/SignatureView/SignatureHelper.swift
+++ Sources/FioriSwiftUICore/SignatureView/SignatureHelper.swift
@@ -40,20 +40,6 @@ func createUIBezierPath(points: [CGPoint]) -> UIBezierPath {
     return bezierPath
 }
 
-extension View {
-    func snapshot() -> UIImage {
-        let controller = UIHostingController(rootView: self)
-        let view = controller.view
-        let targetSize = controller.view.intrinsicContentSize
-        view?.bounds = CGRect(origin: .zero, size: targetSize)
-        view?.backgroundColor = .clear
-        let renderer = UIGraphicsImageRenderer(size: targetSize)
-        return renderer.image { _ in
-            view?.drawHierarchy(in: controller.view.bounds, afterScreenUpdates: true)
-        }
-    }
-}
-
 struct RectGetter: View {
     @Binding var rect: CGRect
 
