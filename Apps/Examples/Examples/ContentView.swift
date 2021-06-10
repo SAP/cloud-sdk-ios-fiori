07c3cbf8bd29ff37b2dd4cb9a2acb4860a73c06b
shengxu7 <61521732+shengxu7@users.noreply.github.com>
2021-06-09 17:14:20 -0700

    fix: improve padding for charts
    * feat: 🎸 improve padding for charts (#288)
    
    * feat: 🎸 make FioriCharts's ifApply() public

--- Apps/Examples/Examples/ContentView.swift
+++ Apps/Examples/Examples/ContentView.swift
@@ -7,12 +7,7 @@ struct ContentView: View {
     private let cards: [String] = ["LowCode", "timeline", "table", "list" , "object", "analytical"]
 
     var body: some View {
-        let linearGradient = LinearGradient(gradient: Gradient(colors: [.red, .green, .blue]), startPoint: .bottom, endPoint: .top)
-        let angularGradient = AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .pink]), center: UnitPoint(x: 0.50, y: 1.00), startAngle: Angle(degrees: 180.00), endAngle: Angle(degrees: 360.00))
-        let radialGradient = RadialGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .pink]), center: .center, startRadius: 0, endRadius: 50)
-        let imagePaint = ImagePaint(image: Image("flower"))
-        
-        return NavigationView {
+        NavigationView {
             List {
                 NavigationLink(
                     destination: ChartsContentView()) {
@@ -30,8 +25,5 @@ struct ContentView: View {
             .navigationBarTitle("Examples")
             .navigationViewStyle(StackNavigationViewStyle())
         }
-        .chartSeriesShapeStyle([0: AnyShapeStyle(linearGradient)])
-        .chartCategoryShapeStyle([0: [0: AnyShapeStyle(linearGradient), 1: AnyShapeStyle(Color.yellow), 2: AnyShapeStyle(angularGradient)],
-                                  1: [3: AnyShapeStyle(radialGradient), 4: AnyShapeStyle(imagePaint)]])
     }
 }
