518e58a471543010bc53ec77f8f8f98fd8fe19e3
xiaoqinggrace <xiaoqinggrace@yahoo.com>
2021-07-27 11:10:20 -0700

    feat: 🎸 User consent form update

--- Sources/FioriSwiftUICore/DataTypes/UserConsent+DataType.swift
+++ Sources/FioriSwiftUICore/DataTypes/UserConsent+DataType.swift
@@ -1,14 +1,17 @@
-
 import SwiftUI
 
+/// Data types for user consent form state
 enum UserConsentState: Int {
     case userConsentInit
     case userConsentCancel
     case userConsentStatus
 }
 
+/// Data types for user consent form
 public struct UserConsentFormData {
+    /// User consent page in the user consent form
     public var pages: [UserConsentPageModel]?
+    /// Boolean denoting whether the user consent form is a mandatory form or not. It is true by default.
     public var isRequired: Bool = true
     public init() {}
 }
