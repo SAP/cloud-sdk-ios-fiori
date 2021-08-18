40f65b2a2d4aef196837549f4432250b9f0d8ddc
Justin Guo <justin.guo@sap.com>
2021-08-18 13:04:49 -0700

    refactor: 💡 address review comments

---
+++ Sources/FioriSwiftUICore/Utils/ControlState.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+/// Data types for `ControlState`. Equivalent to `UIControl.State`.
+public struct ControlState: OptionSet {
+    public let rawValue: UInt
+    
+    /// Initialization
+    public init(rawValue: UInt) { self.rawValue = rawValue }
+    
+    /// Normal UI control state
+    public static let normal = ControlState(rawValue: 1 << 0)
+    /// Highlighted UI control state
+    public static let highlighted = ControlState(rawValue: 1 << 1)
+    /// Disabled UI control state
+    public static let disabled = ControlState(rawValue: 1 << 2)
+    /// Selected UI control state
+    public static let selected = ControlState(rawValue: 1 << 3)
+    /// Focused UI control state
+    public static let focused = ControlState(rawValue: 1 << 4)
+}
+
+extension ControlState: Hashable {
+    /// Extends ``UIControl.State`` to be ``Hashable``
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(self.rawValue)
+    }
+}
