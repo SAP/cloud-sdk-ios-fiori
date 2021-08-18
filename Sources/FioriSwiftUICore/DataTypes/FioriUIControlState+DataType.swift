8f7fefa83e0f7bf826d6fa82ec8928a89a21494e
Justin Guo <justin.guo@sap.com>
2021-08-17 17:17:28 -0700

    refactor: 💡 fix downstream projects build issue

---
+++ Sources/FioriSwiftUICore/DataTypes/FioriUIControlState+DataType.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+/// Data types for `FioriUIControlState`. Equivalent to `UIControl.State`.
+public enum FioriUIControlState {
+    /// Normal UI control state
+    case normal
+    /// Highlighted UI control state
+    case highlighted
+    /// Disabled UI control state
+    case disabled
+    /// Selected UI control state
+    case selected
+    /// Focused UI control state
+    case focused
+}
