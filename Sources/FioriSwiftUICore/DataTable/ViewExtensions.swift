14412d816d7a3946312659ffc037746b51237785
shengxu7 <61521732+shengxu7@users.noreply.github.com>
2021-11-01 15:09:47 -0700

    feat: 🎸 refactor DataTable (#354)

--- Sources/FioriSwiftUICore/DataTable/ViewExtensions.swift
+++ Sources/FioriSwiftUICore/DataTable/ViewExtensions.swift
@@ -37,27 +37,3 @@ struct BlurView: UIViewRepresentable {
     func updateUIView(_ uiView: UIView,
                       context: UIViewRepresentableContext<BlurView>) {}
 }
-
-struct DummyBackground: View {
-    let index: Int
-    let width: CGFloat
-    let height: CGFloat
-    @EnvironmentObject var layoutManager: TableLayoutManager
-    @Environment(\.backgroundColor) var backgroundColor
-    
-    init(index: Int, width: CGFloat, height: CGFloat) {
-        self.index = index
-        self.width = width
-        self.height = height
-    }
-    
-    var body: some View {
-        Group {
-            let zIndex = self.index == 0 ? 700 : 550 - self.index
-            Rectangle()
-                .fill(self.backgroundColor)
-                .frame(width: width, height: height * self.layoutManager.scaleY, alignment: .topLeading)
-                .zIndex(Double(zIndex))
-        }
-    }
-}
