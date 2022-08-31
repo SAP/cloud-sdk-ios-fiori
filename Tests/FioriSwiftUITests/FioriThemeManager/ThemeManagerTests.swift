115b6caa093b8e38959fb2c6945f064317427fc2
c5342342 <kun.zuo@sap.com>
2022-08-31 13:28:55 +0800

    feat:[JIRA:HCPSDKFIORIUIKIT-2210] Add custom colors to FUIColorStyle

--- Tests/FioriSwiftUITests/FioriThemeManager/ThemeManagerTests.swift
+++ Tests/FioriSwiftUITests/FioriThemeManager/ThemeManagerTests.swift
@@ -5,15 +5,15 @@ import XCTest
 
 class ThemeManagerTests: XCTestCase {
     func testLatestColorStyle() throws {
-        XCTAssertEqual(ColorStyle.allCases.count, 188)
+        XCTAssertEqual(ColorStyle.allCases.count, 194)
     }
     
     func testPaletteV7() throws {
         let tm = ThemeManager.shared
         tm.setPaletteVersion(.v7)
-        XCTAssertEqual(tm.paletteVersion?.supportedStyles().count, 190)
+        XCTAssertEqual(tm.paletteVersion?.supportedStyles().count, 196)
         XCTAssertEqual(tm.paletteVersion?.obsoletedStyles().count, 0)
-        XCTAssertEqual(tm.paletteVersion?.newStyles().count, 56)
+        XCTAssertEqual(tm.paletteVersion?.newStyles().count, 62)
         let newStyle_grey1 = tm.hexColor(for: .grey1)
         XCTAssertEqual(newStyle_grey1, HexColor(lightColor: "12171CFF", darkColor: "F5F6F7FF", contrastLightColor: "1C242BFF", contrastDarkColor: "EAECEEFF"))
         let newStyle_primaryLabel = tm.hexColor(for: .primaryLabel)
