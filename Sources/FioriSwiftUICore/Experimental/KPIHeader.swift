7bfbe48cf1fab354c8bd11f320d749bf1228d97f
Xiaoyu Liu <xiaoyuliu0722@gmail.com>
2023-01-16 09:12:18 +0800

    fix: 🐛 address comments

--- Sources/FioriSwiftUICore/Experimental/KPIHeader.swift
+++ Sources/FioriSwiftUICore/Experimental/KPIHeader.swift
@@ -2,20 +2,20 @@ import Combine
 import SwiftUI
 
 public struct KPIHeader<Content: View>: View {
-    private let items: Content
+    private let content: Content
     
-    public init(@KPIHeaderBuilder items: @escaping () -> Content) {
-        self.items = items()
+    public init(@KPIHeaderBuilder content: @escaping () -> Content) {
+        self.content = content()
     }
     
     public var body: some View {
-        items
+        content
     }
 }
 
 public extension KPIHeader {
     /// KPIHeader initialization by data
-    /// - Parameter data: An collection by `KPIHeaderItemModel`
+    /// - Parameter data: An collection by `KPIHeaderItemModel`, maximum 4 items can be rendered.
     init<Data: RandomAccessCollection>(_ data: Data)
         where Content == AnyView, Data.Element == KPIHeaderItemModel, Data.Index == Int
     {
@@ -30,43 +30,13 @@ public extension KPIHeader {
                 }
             }
         }
-        
         @KPIHeaderBuilder
-        func createOneItems() -> some View {
-            makeItem(data[0])
+        func createContent() -> some View {
+            ForEach(0 ..< min(data.count, 4), id: \.self) {
+                makeItem(data[$0])
+            }
         }
         
-        @KPIHeaderBuilder
-        func createTwoItems() -> some View {
-            makeItem(data[0])
-            makeItem(data[1])
-        }
-        
-        @KPIHeaderBuilder
-        func createThreeItems() -> some View {
-            makeItem(data[0])
-            makeItem(data[1])
-            makeItem(data[2])
-        }
-        
-        @KPIHeaderBuilder
-        func createFourItems() -> some View {
-            makeItem(data[0])
-            makeItem(data[1])
-            makeItem(data[2])
-            makeItem(data[3])
-        }
-        
-        if data.count == 1 {
-            self.items = createOneItems().typeErased
-        } else if data.count == 2 {
-            self.items = createTwoItems().typeErased
-        } else if data.count == 3 {
-            self.items = createThreeItems().typeErased
-        } else if data.count >= 4 {
-            self.items = createFourItems().typeErased
-        } else {
-            self.items = EmptyView().typeErased
-        }
+        self.content = createContent().typeErased
     }
 }
