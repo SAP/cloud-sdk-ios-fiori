1abfc097b9603b4ac07c233d03092895c41ef739
Xiaoyu Liu <xiaoyuliu0722@gmail.com>
2023-05-16 07:30:35 +0800

    fix: 🐛 update step item as protocol

--- Sources/FioriSwiftUICore/Views/StepProgressIndicator/StepItem.swift
+++ Sources/FioriSwiftUICore/Views/StepProgressIndicator/StepItem.swift
@@ -1,30 +1,13 @@
 import Foundation
 
 /// Step items data model for `StepProgressIndicator` with a default style.
-public struct StepItem: Identifiable {
-    /// Step id.
-    public var id = UUID().uuidString
+public protocol StepItem {
+    /// Unique step ID.
+    var id: String { get }
     /// Step title.
-    public var title: String?
+    var title: String? { get }
     /// Step state.
-    public var state: StepIndicatorState
-    /// Sub-steps for this one.
-    public var substeps: [StepItem]
-    
-    /// Convenience initialization for step tiem.
-    /// - Parameters:
-    ///   - id: Step id.
-    ///   - title: Step title.
-    ///   - state: Step state.
-    ///   - children: Sub-steps for this one.
-    public init(id: String = UUID().uuidString,
-                title: String? = nil,
-                state: StepIndicatorState = [],
-                substeps: [StepItem] = [])
-    {
-        self.id = id
-        self.title = title
-        self.state = state
-        self.substeps = substeps
-    }
+    var state: StepIndicatorState { get set }
+    /// Substeps for this one.
+    var substeps: [StepItem] { get set }
 }
