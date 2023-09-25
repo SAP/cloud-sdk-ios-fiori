e72034fb97b743b366ba1f8a6fc6d5a32a0dfde7
Bill Zhou <bill.zhou01@sap.com>
2023-09-25 15:01:54 -0700

    fix: 🐛 simultaneous gesture is not triggered in voice over

--- Sources/FioriSwiftUICore/Utils/View+Extensions.swift
+++ Sources/FioriSwiftUICore/Utils/View+Extensions.swift
@@ -3,9 +3,15 @@ import SwiftUI
 
 extension View {
     func onSimultaneousTapGesture(count: Int = 1, perform action: @escaping () -> Void) -> some View {
-        self.simultaneousGesture(TapGesture(count: count).onEnded {
+        self.accessibilitySimultaneousGesture(TapGesture(count: count).onEnded {
             action()
         })
+    }
+    
+    // In order to fix an issue where the simultaneous gesture is not activated in voice over.
+    func accessibilitySimultaneousGesture<T>(_ gesture: T, including mask: GestureMask = .all) -> some View where T: Gesture {
+        simultaneousGesture(gesture, including: mask)
+            .accessibilityElement()
     }
 }
 
@@ -16,27 +22,4 @@ extension View {
             Alert(title: configuration.title, message: configuration.message_, primaryButton: .default(configuration.action.label_, action: configuration.action.didSelect ?? {}), secondaryButton: .cancel(configuration.secondaryAction.label_, action: configuration.secondaryAction.didSelect ?? {}))
         }
     }
-    
-    // Cause problem generating xcframework in SAPFiori
-//    @ViewBuilder
-//    func alert(configuration: AlertConfiguration, isPresented: Binding<Bool>) -> some View {
-//        #if swift(>=5.5)
-//            if #available(iOS 15, macOS 12, *) {
-//                self.alert(configuration.title, isPresented: isPresented) {
-//                    Button(action: configuration.action.didSelect ?? {}, label: { configuration.action.label })
-//                    Button(role: .cancel, action: configuration.secondaryAction.didSelect ?? {}, label: { configuration.secondaryAction.label })
-//                } message: {
-//                    configuration.message
-//                }
-//            } else {
-//                self.alert(isPresented: isPresented) {
-//                    Alert(title: configuration.title, message: configuration.message_, primaryButton: .default(configuration.action.label_, action: configuration.action.didSelect ?? {}), secondaryButton: .cancel(configuration.secondaryAction.label_, action: configuration.secondaryAction.didSelect ?? {}))
-//                }
-//            }
-//        #else
-//            self.alert(isPresented: isPresented) {
-//                Alert(title: configuration.title, message: configuration.message_, primaryButton: .default(configuration.action.label_, action: configuration.action.didSelect ?? {}), secondaryButton: .cancel(configuration.secondaryAction.label_, action: configuration.secondaryAction.didSelect ?? {}))
-//            }
-//        #endif
-//    }
 }
