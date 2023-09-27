cbf58bb69cd41abb6b218606a6dfbcdc3663e2a7
Sheng Xu <sheng.xu01@sap.com>
2023-09-27 13:57:04 -0700

    fix: 🐛 [bcp: 0] fix crashes on VisionOS caused by strokeBorder

--- Sources/FioriCharts/Common/Views/ArcShape.swift
+++ Sources/FioriCharts/Common/Views/ArcShape.swift
@@ -17,10 +17,10 @@ struct ArcShape: InsettableShape {
         let rotationAdjustment = Angle.degrees(90)
         let modifiedStart = self.startAngle - rotationAdjustment
         let modifiedEnd = self.endAngle - rotationAdjustment
-        
+        let radius = min(rect.size.width, rect.size.height) / 2 - self.insetAmount
         var path = Path()
         
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - self.insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !self.clockwise)
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: radius, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !self.clockwise)
         
         return path
     }
@@ -29,28 +29,23 @@ struct ArcShape: InsettableShape {
 struct ArcShape_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            ArcShape(startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 90))
-                .strokeBorder(Color.red)
+            ArcShape(startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 90), insetAmount: 0.5)
+                .stroke(Color.red, lineWidth: 1)
                 .frame(width: 200, height: 200)
                 .previewLayout(.sizeThatFits)
             
-            ArcShape(startAngle: Angle(degrees: 20), endAngle: Angle(degrees: 360))
-                .strokeBorder(Color.red, lineWidth: 40)
+            ArcShape(startAngle: Angle(degrees: 20), endAngle: Angle(degrees: 360), insetAmount: 20)
+                .stroke(Color.red, lineWidth: 40)
+                .frame(width: 200, height: 200)
+                .previewLayout(.sizeThatFits)
+              
+            ArcShape(startAngle: Angle(degrees: 20), endAngle: Angle(degrees: 270), clockwise: false, insetAmount: 50)
+                .stroke(Color.red, lineWidth: 100)
                 .frame(width: 200, height: 200)
                 .previewLayout(.sizeThatFits)
             
-//            ArcShape(startAngle: Angle(degrees: 20), endAngle: Angle(degrees: 360))
-//                .stroke(Color.red, lineWidth: 40)
-//                .frame(width: 200, height: 200)
-//                .previewLayout(.sizeThatFits)
-            
-            ArcShape(startAngle: Angle(degrees: 20), endAngle: Angle(degrees: 270), clockwise: false)
-                .strokeBorder(Color.red, lineWidth: 100)
-                .frame(width: 200, height: 200)
-                .previewLayout(.sizeThatFits)
-            
-            ArcShape(startAngle: Angle(degrees: 20), endAngle: Angle(degrees: 21))
-                .strokeBorder(Color.red, lineWidth: 40)
+            ArcShape(startAngle: Angle(degrees: 20), endAngle: Angle(degrees: 21), insetAmount: 20)
+                .stroke(Color.red, lineWidth: 40)
                 .frame(width: 200, height: 200)
                 .previewLayout(.sizeThatFits)
         }
