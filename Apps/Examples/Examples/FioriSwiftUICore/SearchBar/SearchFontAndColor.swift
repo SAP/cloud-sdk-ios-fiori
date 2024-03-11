e46b31b9163751401de23a6739de8b3c1e4221d1
Alex Xiong <a.xiong@sap.com>
2024-03-11 11:13:28 +0800

    feat: [JIRA: HCPSDKFIORIUIKIT-2497] [New Color System] Search Bar

--- Apps/Examples/Examples/FioriSwiftUICore/SearchBar/SearchFontAndColor.swift
+++ Apps/Examples/Examples/FioriSwiftUICore/SearchBar/SearchFontAndColor.swift
@@ -20,6 +20,20 @@ struct SearchFontAndColor: View {
         .navigationBarTitleDisplayMode(.inline)
         .foregroundColor(.preferredColor(.red5))
         .font(.fiori(forTextStyle: .title3))
+        .onAppear {
+            UISearchTextField.appearance().backgroundColor = UIColor(Color.preferredColor(.quaternaryFill))
+            UISearchTextField.appearance().attributedPlaceholder = NSAttributedString(string: "Color name", attributes: [NSAttributedString.Key.foregroundColor: UIColor(Color.preferredColor(.tertiaryLabel))])
+            
+            let image = UIImage(systemName: "magnifyingglass")?
+                .withTintColor(UIColor(Color.preferredColor(.red4)), renderingMode: .alwaysOriginal)
+                .applyingSymbolConfiguration(UIImage.SymbolConfiguration(weight: .semibold))
+            UISearchBar.appearance().setImage(image, for: .search, state: .normal)
+        }
+        .onDisappear {
+            UISearchTextField.appearance().backgroundColor = nil
+            UISearchTextField.appearance().attributedPlaceholder = nil
+            UISearchBar.appearance().setImage(nil, for: .search, state: .normal)
+        }
     }
 }
 
