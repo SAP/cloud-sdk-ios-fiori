0ca8a01a5e27a0a40cd12662a8542686497c596e
hengyi-zhang <hengyizhangcn@gmail.com>
2024-06-25 03:09:16 +0800

    feat: 🎸 [JIRA:IOSSDKBUG-5] add variables to ChartLabelAttributes (#723)
    add two variables(font and backgroundColor) to ChartLabelAttributes, for
    user to custom chart label attributes conveniently
    
    ✅ Closes: #692

--- Sources/FioriCharts/Model/ChartLabelAttributes.swift
+++ Sources/FioriCharts/Model/ChartLabelAttributes.swift
@@ -1,3 +1,5 @@
+import Combine
+import FioriThemeManager
 import Foundation
 import SwiftUI
 
@@ -17,24 +19,59 @@ public class ChartLabelAttributes: ObservableObject, Identifiable, NSCopying {
     /// True when the associated label(s) should be hidden.
     @Published public var isHidden: Bool
     
+    /// Font for the label, has higher priority than fontSize.
+    @Published public var font: Font?
+    /// Backgroundcolor for the label.
+    @Published public var backgroundColor: Color?
+    
+    /// When font not be configured, use .fiori(fixedSize: fontSize) as default;
+    var targetFont: Font {
+        if let font {
+            return font
+        } else {
+            return .fiori(fixedSize: self.fontSize)
+        }
+    }
+    
     public let id = UUID()
     
+    /// The constructor with color, fontSize, offset, isHidden and backgroundColor
     public init(color: Color = .preferredColor(.tertiaryLabel),
                 fontSize: Double = 12,
                 offset: Double = 0,
-                isHidden: Bool = false)
+                isHidden: Bool = false,
+                backgroundColor: Color? = nil)
     {
         self._color = Published(initialValue: color)
         self._fontSize = PublishedConstrainedValue(wrappedValue: CGFloat(fontSize), 0 ... 100)
         self._offset = PublishedConstrainedValue(wrappedValue: CGFloat(offset), -10 ... 10)
         self._isHidden = Published(initialValue: isHidden)
+        self._backgroundColor = Published(initialValue: backgroundColor)
+    }
+    
+    /// The constructor with color, font, offset, isHidden and backgroundColor
+    public init(color: Color = .preferredColor(.tertiaryLabel),
+                font: Font?,
+                offset: Double = 0,
+                isHidden: Bool = false,
+                backgroundColor: Color? = nil)
+    {
+        self._color = Published(initialValue: color)
+        self._font = Published(initialValue: font)
+        self._offset = PublishedConstrainedValue(wrappedValue: CGFloat(offset), -10 ... 10)
+        self._isHidden = Published(initialValue: isHidden)
+        self._backgroundColor = Published(initialValue: backgroundColor)
     }
         
     public func copy(with zone: NSZone? = nil) -> Any {
-        ChartLabelAttributes(color: self.color,
-                             fontSize: Double(self.fontSize),
-                             offset: Double(self.offset),
-                             isHidden: self.isHidden)
+        let chartLabelAttributes = ChartLabelAttributes(color: self.color,
+                                                        fontSize: Double(self.fontSize),
+                                                        offset: Double(self.offset),
+                                                        isHidden: self.isHidden,
+                                                        backgroundColor: self.backgroundColor)
+        // assign font property
+        chartLabelAttributes.font = self.font
+        return chartLabelAttributes
     }
 }
 
@@ -43,7 +80,9 @@ extension ChartLabelAttributes: Equatable {
         lhs.color == rhs.color &&
             lhs.fontSize == rhs.fontSize &&
             lhs.offset == rhs.offset &&
-            lhs.isHidden == rhs.isHidden
+            lhs.isHidden == rhs.isHidden &&
+            lhs.font == rhs.font &&
+            lhs.backgroundColor == rhs.backgroundColor
     }
 }
 
