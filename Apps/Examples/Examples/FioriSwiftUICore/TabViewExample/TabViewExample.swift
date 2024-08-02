70db47dc1737deb63abdb9a032887d2afb2af40d
xiaoyu0722 <xiaoyuliu0722@gmail.com>
2024-08-02 09:04:26 +0800

    fix: 🐛 tab bar styles

--- Apps/Examples/Examples/FioriSwiftUICore/TabViewExample/TabViewExample.swift
+++ Apps/Examples/Examples/FioriSwiftUICore/TabViewExample/TabViewExample.swift
@@ -8,12 +8,13 @@ struct TabViewExample: View {
     @State var isCustomColor: Bool = false
     @State var isCustomSelectionIndicatorImage: Bool = false
     @State var isCustomBackgroundImage: Bool = false
+    @State var isSystemTabView: Bool = false
 
     var body: some View {
         Form {
             if self.isModalPresentation {
                 HStack {
-                    Text("Selecte to Test")
+                    Text("Select to Test")
                     Spacer()
                 }
                 .contentShape(Rectangle())
@@ -21,19 +22,21 @@ struct TabViewExample: View {
                     self.isPresented.toggle()
                 }
                 .sheet(isPresented: self.$isPresented) {
-                    TabViewDetailView(numberOfTabs: self.$numberOfTabs,
-                                      isCustomColor: self.$isCustomColor,
-                                      isCustomSelectionIndicatorImage: self.$isCustomSelectionIndicatorImage,
-                                      isCustomBackgroundImage: self.$isCustomBackgroundImage)
+                    TabViewDetailView(numberOfTabs: self.numberOfTabs,
+                                      isCustomColor: self.isCustomColor,
+                                      isCustomSelectionIndicatorImage: self.isCustomSelectionIndicatorImage,
+                                      isCustomBackgroundImage: self.isCustomBackgroundImage,
+                                      isSystemTabView: self.isSystemTabView)
                 }
             } else {
                 NavigationLink {
-                    TabViewDetailView(numberOfTabs: self.$numberOfTabs,
-                                      isCustomColor: self.$isCustomColor,
-                                      isCustomSelectionIndicatorImage: self.$isCustomSelectionIndicatorImage,
-                                      isCustomBackgroundImage: self.$isCustomBackgroundImage)
+                    TabViewDetailView(numberOfTabs: self.numberOfTabs,
+                                      isCustomColor: self.isCustomColor,
+                                      isCustomSelectionIndicatorImage: self.isCustomSelectionIndicatorImage,
+                                      isCustomBackgroundImage: self.isCustomBackgroundImage,
+                                      isSystemTabView: self.isSystemTabView)
                 } label: {
-                    Text("Selecte to Test")
+                    Text("Select to Test")
                 }
             }
             
@@ -46,11 +49,12 @@ struct TabViewExample: View {
             }
             
             Toggle("Modal Presentation", isOn: self.$isModalPresentation)
-            Toggle("Custom Colors", isOn: self.$isCustomColor)
+            Toggle("Custom Colors and Font Size", isOn: self.$isCustomColor)
             Toggle("Custom Selection Indicator Image", isOn: self.$isCustomSelectionIndicatorImage)
             Toggle("Custom Background Image", isOn: self.$isCustomBackgroundImage)
+            Toggle("SwiftUI TabView", isOn: self.$isSystemTabView)
         }
-        .navigationTitle("TabView Example")
+        .navigationTitle("TabView Options")
     }
 }
 
