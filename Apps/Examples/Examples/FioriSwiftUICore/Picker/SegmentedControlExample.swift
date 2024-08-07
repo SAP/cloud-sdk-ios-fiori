19aced20b11613b9bf0205e158ac75eb2e360eea
angiexyang <yang.angiex@gmail.com>
2024-08-07 14:42:52 -0700

    test: 💍 custom styling

--- Apps/Examples/Examples/FioriSwiftUICore/Picker/SegmentedControlExample.swift
+++ Apps/Examples/Examples/FioriSwiftUICore/Picker/SegmentedControlExample.swift
@@ -4,10 +4,36 @@ import SwiftUI
 
 struct SegmentedControlExample: View {
     var segments: [AttributedString] = ["Segment 1", "Segment 2", "Segment 3"]
-    @State var selectedIdx1: Int = 0
+    @State var selectedIndex: Int = 0
+    @State private var _isCustomStyle = false
+
+    struct CustomStyle: SegmentedControlPickerStyle {
+        func makeBody(_ configuration: SegmentedControlPickerConfiguration) -> some View {
+            let appearance = UISegmentedControl.appearance()
+            appearance.selectedSegmentTintColor = UIColor.systemIndigo
+            appearance.backgroundColor = UIColor.systemPurple
+            appearance.setTitleTextAttributes([.foregroundColor: UIColor.white, .font: UIFont.preferredFioriFont(forTextStyle: .body, weight: .bold)], for: .selected)
+            appearance.setTitleTextAttributes([.foregroundColor: UIColor.white, .font: UIFont.preferredFioriFont(forTextStyle: .body)], for: .normal)
+            
+            return SegmentedControlPicker(configuration)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 9)
+                        .stroke(Color.blue, lineWidth: 3)
+                )
+        }
+    }
     
     var body: some View {
-        SegmentedControlPicker(options: self.segments, selectedIndex: self.$selectedIdx1)
+        VStack {
+            Toggle("Custom style", isOn: self.$_isCustomStyle).padding()
+            Spacer()
+            if self._isCustomStyle {
+                SegmentedControlPicker(options: self.segments, selectedIndex: self.$selectedIndex).segmentedControlPickerStyle(CustomStyle())
+            } else {
+                SegmentedControlPicker(options: self.segments, selectedIndex: self.$selectedIndex)
+            }
+            Spacer()
+        }
     }
 }
     
