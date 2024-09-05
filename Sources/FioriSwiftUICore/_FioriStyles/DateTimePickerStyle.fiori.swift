b28df881f7cd5d9594c8bc6e7e554436384c3051
angiexyang <yang.angiex@gmail.com>
2024-09-05 12:02:36 -0700

    docs: ✏️ clean up comments

--- Sources/FioriSwiftUICore/_FioriStyles/DateTimePickerStyle.fiori.swift
+++ Sources/FioriSwiftUICore/_FioriStyles/DateTimePickerStyle.fiori.swift
@@ -1,18 +1,6 @@
 import FioriThemeManager
-
-// Generated using Sourcery 2.1.7 — https://github.com/krzysztofzablocki/Sourcery
-// DO NOT EDIT
 import Foundation
 import SwiftUI
-
-/**
- This file provides default fiori style for the component.
-
- 1. Uncomment fhe following code.
- 2. Implement layout and style in corresponding places.
- 3. Delete `.generated` from file name.
- 4. Move this file to `_FioriStyles` folder under `FioriSwiftUICore`.
- */
 
 // Base Layout style
 public struct DateTimePickerBaseStyle: DateTimePickerStyle {
@@ -20,7 +8,6 @@ public struct DateTimePickerBaseStyle: DateTimePickerStyle {
     @State var pickerVisible: Bool = false
     
     public func makeBody(_ configuration: DateTimePickerConfiguration) -> some View {
-        // Add default layout here
         VStack {
             HStack {
                 configuration.title
@@ -73,8 +60,6 @@ extension DateTimePickerFioriStyle {
     struct ContentFioriStyle: DateTimePickerStyle {
         func makeBody(_ configuration: DateTimePickerConfiguration) -> some View {
             DateTimePicker(configuration)
-            // Add default style for its content
-            // .background()
         }
     }
 
@@ -83,7 +68,6 @@ extension DateTimePickerFioriStyle {
 
         func makeBody(_ configuration: TitleConfiguration) -> some View {
             Title(configuration)
-                // Add default style for Title
                 .foregroundStyle(Color.preferredColor(.primaryLabel))
                 .font(.fiori(forTextStyle: .subheadline, weight: .semibold))
         }
