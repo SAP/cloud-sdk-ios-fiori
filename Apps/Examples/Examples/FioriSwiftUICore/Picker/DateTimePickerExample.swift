f3d97404ed2bb57df496ec2a73ece6b645d3dcf5
I824136 <xiaoqing.he@sap.com>
2024-10-01 21:43:45 -0700

    feat: 🎸 HCPSDKFIORIUIKIT-2718 Mandatory Field for DateTimePicker
    Add Mandatory Field Styling for DateTimePicker. Add controlState for
    disable/readonly state. Add informationView for the error/hint message.

--- Apps/Examples/Examples/FioriSwiftUICore/Picker/DateTimePickerExample.swift
+++ Apps/Examples/Examples/FioriSwiftUICore/Picker/DateTimePickerExample.swift
@@ -3,14 +3,25 @@ import Foundation
 import SwiftUI
 
 struct DateTimePickerExample: View {
-    @State var s1: Date = .init()
+    @State var s1: Date = .init(timeIntervalSince1970: 0.0)
     @State var s2: Date = .init()
     @State var s3: Date = .init()
     @State var s4: Date = .init()
+    @State var s5: Date = .now
+    @State var isRequired = false
+    @State var showsErrorMessage = false
     
     struct CustomTitleStyle: TitleStyle {
         func makeBody(_ configuration: TitleConfiguration) -> some View {
             Title(configuration)
+                .font(.fiori(forTextStyle: .title3))
+                .foregroundStyle(Color.preferredColor(.indigo7))
+        }
+    }
+    
+    struct CustomIndicatorStyle: MandatoryFieldIndicatorStyle {
+        func makeBody(_ configuration: MandatoryFieldIndicatorConfiguration) -> some View {
+            MandatoryFieldIndicator(configuration)
                 .font(.fiori(forTextStyle: .title3))
                 .foregroundStyle(Color.preferredColor(.indigo7))
         }
@@ -25,13 +36,27 @@ struct DateTimePickerExample: View {
     }
     
     var body: some View {
-        VStack {
-            DateTimePicker(title: "Default", selectedDate: self.$s1)
-            DateTimePicker(title: "Date only", selectedDate: self.$s2, pickerComponents: [.date])
-            DateTimePicker(title: "Time only", selectedDate: self.$s3, pickerComponents: [.hourAndMinute])
-            DateTimePicker(title: "Custom Style", selectedDate: self.$s3)
+        List {
+            Toggle("Mandatory Field", isOn: self.$isRequired)
+            Toggle("Show Error/Hint message", isOn: self.$showsErrorMessage)
+            
+            DateTimePicker(title: "Default", isRequired: self.isRequired, selectedDate: self.$s1)
+                .informationView(isPresented: self.$showsErrorMessage, description: AttributedString("The Date should before December."))
+                .informationViewStyle(.informational)
+            DateTimePicker(title: "Date only", isRequired: self.isRequired, selectedDate: self.$s2, pickerComponents: [.date])
+                .informationView(isPresented: self.$showsErrorMessage, description: AttributedString("The Date should before December."))
+                .informationViewStyle(.error)
+            DateTimePicker(title: "Time only", isRequired: self.isRequired, selectedDate: self.$s3, pickerComponents: [.hourAndMinute])
+         
+            DateTimePicker(title: "Custom Style", isRequired: self.isRequired, selectedDate: self.$s4)
                 .titleStyle(CustomTitleStyle())
+                .mandatoryFieldIndicatorStyle(CustomIndicatorStyle())
                 .valueLabelStyle(CustomValueLabelStyle())
+            Text("Disabled")
+            DateTimePicker(title: "In Disabled Mode", controlState: .disabled, selectedDate: self.$s5)
+                .disabled(true)
+            Text("Read-Only")
+            DateTimePicker(title: "In Read-Only Mode", controlState: .readOnly, selectedDate: self.$s5, pickerComponents: [.date])
         }
     }
 }
