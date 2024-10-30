359d846bf67f56d47fa87a8f619abf6979b4805f
zzchao-1999 <149659707+zzchao-1999@users.noreply.github.com>
2024-10-30 23:33:01 +0800

    feat: 🎸 [HCPSDKFIORIUIKIT-2791]SwiftUI: floating value support (#834) (#849)
    * feat: 🎸 [HCPSDKFIORIUIKIT-2791]SwiftUI: floating value support
    
    * feat: 🎸 [HCPSDKFIORIUIKIT-2791]Added a number formatter

--- Sources/FioriSwiftUICore/_generated/StyleableComponents/StepperView/StepperView.generated.swift
+++ Sources/FioriSwiftUICore/_generated/StyleableComponents/StepperView/StepperView.generated.swift
@@ -11,9 +11,11 @@ public struct StepperView {
     @Binding var text: String
     let incrementAction: any View
     /// The step value
-    let step: Int
+    let step: Double
     /// a range of values
-    let stepRange: ClosedRange<Int>
+    let stepRange: ClosedRange<Double>
+    /// Indicates whether the stepper field  supports decimal values. Default is false.
+    let isDecimalSupported: Bool
     let icon: any View
     let description: any View
 
@@ -25,8 +27,9 @@ public struct StepperView {
                 @ViewBuilder decrementAction: () -> any View = { FioriButton { _ in FioriIcon.actions.less } },
                 text: Binding<String>,
                 @ViewBuilder incrementAction: () -> any View = { FioriButton { _ in FioriIcon.actions.add } },
-                step: Int = 1,
-                stepRange: ClosedRange<Int>,
+                step: Double = 1,
+                stepRange: ClosedRange<Double>,
+                isDecimalSupported: Bool = false,
                 @ViewBuilder icon: () -> any View = { EmptyView() },
                 @ViewBuilder description: () -> any View = { EmptyView() })
     {
@@ -36,8 +39,9 @@ public struct StepperView {
         self.incrementAction = IncrementAction { incrementAction() }
         self.step = step
         self.stepRange = stepRange
-        self.icon = Icon { icon() }
-        self.description = Description { description() }
+        self.isDecimalSupported = isDecimalSupported
+        self.icon = Icon(icon: icon)
+        self.description = Description(description: description)
     }
 }
 
@@ -46,12 +50,13 @@ public extension StepperView {
          decrementAction: FioriButton? = FioriButton { _ in FioriIcon.actions.less },
          text: Binding<String>,
          incrementAction: FioriButton? = FioriButton { _ in FioriIcon.actions.add },
-         step: Int = 1,
-         stepRange: ClosedRange<Int>,
+         step: Double = 1,
+         stepRange: ClosedRange<Double>,
+         isDecimalSupported: Bool = false,
          icon: Image? = nil,
          description: AttributedString? = nil)
     {
-        self.init(title: { Text(title) }, decrementAction: { decrementAction }, text: text, incrementAction: { incrementAction }, step: step, stepRange: stepRange, icon: { icon }, description: { OptionalText(description) })
+        self.init(title: { Text(title) }, decrementAction: { decrementAction }, text: text, incrementAction: { incrementAction }, step: step, stepRange: stepRange, isDecimalSupported: isDecimalSupported, icon: { icon }, description: { OptionalText(description) })
     }
 }
 
@@ -67,6 +72,7 @@ public extension StepperView {
         self.incrementAction = configuration.incrementAction
         self.step = configuration.step
         self.stepRange = configuration.stepRange
+        self.isDecimalSupported = configuration.isDecimalSupported
         self.icon = configuration.icon
         self.description = configuration.description
         self._shouldApplyDefaultStyle = shouldApplyDefaultStyle
@@ -78,7 +84,7 @@ extension StepperView: View {
         if self._shouldApplyDefaultStyle {
             self.defaultStyle()
         } else {
-            self.style.resolve(configuration: .init(title: .init(self.title), decrementAction: .init(self.decrementAction), text: self.$text, incrementAction: .init(self.incrementAction), step: self.step, stepRange: self.stepRange, icon: .init(self.icon), description: .init(self.description))).typeErased
+            self.style.resolve(configuration: .init(title: .init(self.title), decrementAction: .init(self.decrementAction), text: self.$text, incrementAction: .init(self.incrementAction), step: self.step, stepRange: self.stepRange, isDecimalSupported: self.isDecimalSupported, icon: .init(self.icon), description: .init(self.description))).typeErased
                 .transformEnvironment(\.stepperViewStyleStack) { stack in
                     if !stack.isEmpty {
                         stack.removeLast()
@@ -96,7 +102,7 @@ private extension StepperView {
     }
 
     func defaultStyle() -> some View {
-        StepperView(.init(title: .init(self.title), decrementAction: .init(self.decrementAction), text: self.$text, incrementAction: .init(self.incrementAction), step: self.step, stepRange: self.stepRange, icon: .init(self.icon), description: .init(self.description)))
+        StepperView(.init(title: .init(self.title), decrementAction: .init(self.decrementAction), text: self.$text, incrementAction: .init(self.incrementAction), step: self.step, stepRange: self.stepRange, isDecimalSupported: self.isDecimalSupported, icon: .init(self.icon), description: .init(self.description)))
             .shouldApplyDefaultStyle(false)
             .stepperViewStyle(StepperViewFioriStyle.ContentFioriStyle())
             .typeErased
