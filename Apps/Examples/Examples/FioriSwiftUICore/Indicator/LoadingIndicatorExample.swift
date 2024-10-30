054bc45cba455f6fad2c895e09fa0206df8a882c
Zuo, Kun <kun.zuo@sap.com>
2024-10-30 10:13:08 +0800

    feat: 🎸 [JIRA:HCPSDKFIORIUIKIT-2796]Loading Indicator
    Modify test example

--- Apps/Examples/Examples/FioriSwiftUICore/Indicator/LoadingIndicatorExample.swift
+++ Apps/Examples/Examples/FioriSwiftUICore/Indicator/LoadingIndicatorExample.swift
@@ -10,7 +10,14 @@ struct LoadingIndicatorExample: View {
     @State var isPresented5: Bool = true
     @State var isPresented6: Bool = true
     @State var isPresented7: Bool = true
-    
+    var message: AttributedString {
+        var result = AttributedString("bottom indicator")
+        result.font = .largeTitle
+        result.foregroundColor = .white
+        result.backgroundColor = .blue
+        return result
+    }
+
     var body: some View {
         List {
             Section("Default Loading Indicator") {
@@ -19,7 +26,7 @@ struct LoadingIndicatorExample: View {
                     .multilineTextAlignment(.center)
                     .padding()
             }.headerProminence(.increased)
-            
+
             Section("Based On The Use Of Overlay") {
                 VStack {
                     Rectangle()
@@ -39,47 +46,65 @@ struct LoadingIndicatorExample: View {
                     }.buttonStyle(.borderedProminent)
                 }
             }.headerProminence(.increased)
-            
-            Section("Alignments And Customized") {
-                VStack(alignment: .center) {
-                    HStack(alignment: .center) {
-                        LoadingIndicatorView(title: "mini size", isPresented: self.$isPresented3)
-                            .indicatorPosition(.trailing)
-                            .indicatorTint(Color.random)
-                            .indicatorControlSize(.mini)
-                        Spacer()
-                        LoadingIndicatorView(title: {
-                            Text("small size")
-                                .font(.largeTitle)
-                                .foregroundStyle(Color.green)
-                        }, isPresented: self.$isPresented4)
+            Section("Control Size") {
+                VStack(alignment: .leading) {
+                    LoadingIndicatorView(title: "mini size indicator", isPresented: self.$isPresented3)
+                        .indicatorPosition(.leading)
+                        .indicatorControlSize(.mini)
+                    LoadingIndicatorView(title: "small size indicator", isPresented: self.$isPresented3)
+                        .indicatorPosition(.leading)
+                        .indicatorControlSize(.small)
+                    LoadingIndicatorView(title: "regular size indicator", isPresented: self.$isPresented3)
+                        .indicatorPosition(.leading)
+                        .indicatorControlSize(.regular)
+                    LoadingIndicatorView(title: "large size indicator", isPresented: self.$isPresented6)
+                        .indicatorPosition(.leading)
+                        .indicatorControlSize(.large)
+                    if #available(iOS 17.0, *) {
+                        LoadingIndicatorView(title: "extraLarge size indicator", isPresented: self.$isPresented6)
                             .indicatorPosition(.leading)
-                            .indicatorTint(Color.random)
-                            .indicatorControlSize(.small)
-                    }.padding()
-                    HStack(alignment: .center) {
-                        if #available(iOS 17.0, *) {
-                            LoadingIndicatorView(title: {
-                                Text("extraLarge size")
-                                    .font(.system(size: 8))
-                                    .foregroundStyle(Color.red)
-                            }, isPresented: self.$isPresented5)
-                                .indicatorPosition(.top)
-                                .indicatorTint(Color.random)
-                                .indicatorControlSize(.extraLarge)
-                        } else {
-                            // Fallback on earlier versions
-                        }
-                        Spacer()
-                        LoadingIndicatorView(title: "large size", isPresented: self.$isPresented6)
-                            .indicatorPosition(.bottom)
-                            .indicatorTint(Color.random)
-                            .indicatorControlSize(.large)
-                    }.padding()
+                            .indicatorControlSize(.extraLarge)
+                    } else {
+                        // Fallback on earlier versions
+                    }
                 }
+            }.headerProminence(.increased)
+            Section("Position And Custom Text") {
+                VStack(alignment: .center) {
+                    LoadingIndicatorView(title: "leading indicator", isPresented: self.$isPresented3)
+                        .indicatorPosition(.leading)
+                        .indicatorTint(Color.random)
+                    Spacer()
+                    if #available(iOS 17.0, *) {
+                        LoadingIndicatorView(title: {
+                            Text("top indicator")
+                                .font(.system(size: 8))
+                                .foregroundStyle(Color.red)
+                        }, isPresented: self.$isPresented5)
+                            .indicatorPosition(.top)
+                            .indicatorTint(Color.random)
+                    } else {
+                        // Fallback on earlier versions
+                    }
+                    Spacer()
+                    LoadingIndicatorView(title: {
+                        Text("trailing indicator")
+                            .font(.largeTitle)
+                            .foregroundStyle(Color.green)
+                    }, isPresented: self.$isPresented4)
+                        .indicatorPosition(.trailing)
+                        .indicatorTint(Color.random)
+                    Spacer()
+                    LoadingIndicatorView(title: self.message, isPresented: self.$isPresented6)
+                        .indicatorPosition(.bottom)
+                        .indicatorTint(Color.random)
+                }
+                .frame(maxWidth: .infinity)
+                .multilineTextAlignment(.center)
+                .padding()
             }
             .headerProminence(.increased)
-            
+
             Section("Delay 10 Seconds Dismiss") {
                 VStack {
                     LoadingIndicatorView(title: "Loading...", duration: 10, isPresented: self.$isPresented7)
