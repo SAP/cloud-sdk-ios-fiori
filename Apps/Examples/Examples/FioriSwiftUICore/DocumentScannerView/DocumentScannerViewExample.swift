71df382cc26dc4951a775a950d3a579bb6e894ec
Ze-Chao Zhang <149659707+zzchao-1999@users.noreply.github.com>
2024-11-04 10:59:29 +0800

    feat: 🎸 HCPSDKFIORIUIKIT-2709 Address the new comments

--- Apps/Examples/Examples/FioriSwiftUICore/DocumentScannerView/DocumentScannerViewExample.swift
+++ Apps/Examples/Examples/FioriSwiftUICore/DocumentScannerView/DocumentScannerViewExample.swift
@@ -8,45 +8,59 @@ struct DocumentScannerViewExample: View {
     @State private var pdfDocument: PDFDocument?
     @State private var isPDFDocument: Bool = false
     @State private var pdfIdentifier = 0
+    @State private var viewerTitle: String = "Results as Images"
     
     var body: some View {
         VStack {
-            Toggle("Scan PDF Document", isOn: self.$isPDFDocument)
+            Toggle("Save Results as PDF", isOn: self.$isPDFDocument)
                 .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
-            ScrollView {
-                LazyVStack(content: {
-                    if self.isPDFDocument, let pdfDoc = self.pdfDocument {
-                        Text("PDF Viewer")
-                            .foregroundColor(.accentColor)
-                        PDFKitView(pdfDocument: pdfDoc, id: self.pdfIdentifier)
-                            .scaledToFill()
+                .onChange(of: self.isPDFDocument) { _ in
+                    if self.isPDFDocument {
+                        self.viewerTitle = "Results as PDF"
                     } else {
-                        ForEach(self.scannedImages, id: \.self) { image in
-                            Image(uiImage: image)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 300, height: 300)
-                        }
+                        self.viewerTitle = "Results as Images"
                     }
-                })
-                .background(Color.preferredColor(.grey1))
-            }
-        }
-        .navigationTitle("Document Scanner")
-        .toolbar {
-            ToolbarItem(placement: .topBarTrailing) {
+                }
+            VStack(alignment: .center) {
                 Button {
                     self.isScannerPresented.toggle()
                 } label: {
                     Image(systemName: "doc.viewfinder")
-                        .resizable()
-                        .scaledToFit()
                         .foregroundColor(.blue)
+                        .font(.largeTitle)
+                        .frame(width: 40, height: 40)
                 }
                 .buttonStyle(.plain)
                 .fontWeight(.bold)
+                Text("Scan to Capture Documents")
+                    .font(.footnote)
             }
+            ScrollView {
+                Text(self.viewerTitle)
+                    .font(.subheadline)
+                    .foregroundColor(.accentColor)
+                Group {
+                    if self.isPDFDocument, let pdfDoc = self.pdfDocument {
+                        PDFViewer(pdfDocument: pdfDoc, id: self.pdfIdentifier)
+                            .scaledToFill()
+                    } else {
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(alignment: .center) {
+                                ForEach(self.scannedImages, id: \.self) { image in
+                                    Image(uiImage: image)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 300, height: 300)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .background(Color.preferredColor(.grey1))
+            .frame(minHeight: 300)
         }
+        .navigationTitle("Document Scanner")
         .fullScreenCover(isPresented: self.$isScannerPresented) {
             DocumentScannerView(onCompletion: { result in
                 switch result {
@@ -65,11 +79,11 @@ struct DocumentScannerViewExample: View {
     }
 }
 
-struct PDFKitView: UIViewRepresentable {
+struct PDFViewer: UIViewRepresentable {
     let pdfDocument: PDFDocument
     let id: Int
     
-    func makeUIView(context: UIViewRepresentableContext<PDFKitView>) -> PDFView {
+    func makeUIView(context: UIViewRepresentableContext<PDFViewer>) -> PDFView {
         let pdfView = PDFView()
         pdfView.document = self.pdfDocument
         return pdfView
