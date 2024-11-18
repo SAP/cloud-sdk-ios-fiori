1a7ff578f2ccee9f1e91b4464f17df54ab0927e6
restaurantt <restaurantt@gmail.com>
2024-11-19 03:04:23 +0800

    fix: 🐛 [IOSSDKBUG-454] FilterFeedbackBar dark mode design (#889)
    * fix: 🐛 [IOSSDKBUG-454] FilterFeedbackBar dark mode design
    
    * fix: 🐛 [IOSSDKBUG-454] FilterFeedbackBar dark mode design
    
    ---------
    
    Co-authored-by: dyongxu <61523257+dyongxu@users.noreply.github.com>

--- Sources/FioriSwiftUICore/Views/SearchListPickerItem+View.swift
+++ Sources/FioriSwiftUICore/Views/SearchListPickerItem+View.swift
@@ -43,7 +43,8 @@ extension SearchListPickerItem: View {
                                 _onTap?(index)
                             }
                     }
-                    
+                    .listRowBackground(Color.preferredColor(.chromeSecondary))
+
                     Rectangle().fill(Color.preferredColor(.primaryGroupedBackground))
                         .frame(height: 30)
                         .listRowInsets(EdgeInsets())
@@ -72,6 +73,7 @@ extension SearchListPickerItem: View {
                             _onTap?(index)
                         }
                 }
+                .listRowBackground(Color.preferredColor(.chromeSecondary))
             }
         }
         .modifier(FioriIntrospectModifier<UIScrollView> { scrollView in
