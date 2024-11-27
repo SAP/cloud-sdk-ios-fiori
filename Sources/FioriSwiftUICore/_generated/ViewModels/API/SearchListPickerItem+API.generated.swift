28d714fcebcc4067152aa23b45bc295fb7b3391c
Gu, Jiajun (external - Project) <restaurantt@gmail.com>
2024-11-27 09:44:19 +0800

    fix: 🐛 [IOSSDKBUG-480]Popover view height on iPad

--- Sources/FioriSwiftUICore/_generated/ViewModels/API/SearchListPickerItem+API.generated.swift
+++ Sources/FioriSwiftUICore/_generated/ViewModels/API/SearchListPickerItem+API.generated.swift
@@ -9,18 +9,19 @@ public struct SearchListPickerItem {
 	var _valueOptions: [String]
 	var _hint: String? = nil
 	var _onTap: ((_ index: Int) -> Void)? = nil
-
+    
+	var barItemFrame: CGRect = .zero
 	var disableListEntriesSection: Bool = false
-	var updateSearchListPickerHeight: ((CGFloat) -> ())? = nil
+	var isSearchBarHidden: Bool = false
+	var allowsMultipleSelection: Bool = false
+	var allowsEmptySelection: Bool = false
+	@State var _searchViewCornerRadius: CGFloat = 18
 	var selectAll: ((Bool) -> ())? = nil
 	let popoverWidth = 393.0
-	@State var _height: CGFloat = 44
-	@State var _searchViewCornerRadius: CGFloat = 18
-	@State var _searchText: String = ""
-	var allowsEmptySelection: Bool = false
-	var allowsMultipleSelection: Bool = false
 	var allowsDisplaySelectionCount: Bool = true
-	var isSearchBarHidden: Bool = false
+	@State var _searchText: String = ""
+	var updateSearchListPickerHeight: ((CGFloat) -> ())? = nil
+	@State var _height: CGFloat = 44
 	@State var _keyboardHeight: CGFloat = 0.0
 
     public init(model: SearchListPickerItemModel) {
