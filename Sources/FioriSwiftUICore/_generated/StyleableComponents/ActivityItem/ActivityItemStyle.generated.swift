9dff8802a47b6d9722a43f86e83cf624d9d978c2
hengyi-zhang <hengyizhangcn@gmail.com>
2024-12-14 02:07:40 +0800

    feat: 🎸 [JIRA:HCPSDKFIORIUIKIT-2880] ActivityItemModel Refactor (#928)
    * feat: 🎸 [JIRA:HCPSDKFIORIUIKIT-2880] ActivityItemModel Refactor
    
    * feat: 🎸 [JIRA:HCPSDKFIORIUIKIT-2880] ActivityItemModel Refactor
    
    * feat: 🎸 [JIRA:HCPSDKFIORIUIKIT-2880] ActivityItemModel Refactor
    
    ---------
    
    Co-authored-by: Bill Zhou <bill.zhou01@sap.com>
    Co-authored-by: dyongxu <61523257+dyongxu@users.noreply.github.com>

---
+++ Sources/FioriSwiftUICore/_generated/StyleableComponents/ActivityItem/ActivityItemStyle.generated.swift
@@ -0,0 +1,39 @@
+// Generated using Sourcery 2.1.7 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+import Foundation
+import SwiftUI
+
+public protocol ActivityItemStyle: DynamicProperty {
+    associatedtype Body: View
+
+    func makeBody(_ configuration: ActivityItemConfiguration) -> Body
+}
+
+struct AnyActivityItemStyle: ActivityItemStyle {
+    let content: (ActivityItemConfiguration) -> any View
+
+    init(@ViewBuilder _ content: @escaping (ActivityItemConfiguration) -> any View) {
+        self.content = content
+    }
+
+    public func makeBody(_ configuration: ActivityItemConfiguration) -> some View {
+        self.content(configuration).typeErased
+    }
+}
+
+public struct ActivityItemConfiguration {
+    public let icon: Icon
+    public let subtitle: Subtitle
+    public let layout: ActivityItemLayout
+
+    public typealias Icon = ConfigurationViewWrapper
+    public typealias Subtitle = ConfigurationViewWrapper
+}
+
+public struct ActivityItemFioriStyle: ActivityItemStyle {
+    public func makeBody(_ configuration: ActivityItemConfiguration) -> some View {
+        ActivityItem(configuration)
+            .iconStyle(IconFioriStyle(activityItemConfiguration: configuration))
+            .subtitleStyle(SubtitleFioriStyle(activityItemConfiguration: configuration))
+    }
+}
