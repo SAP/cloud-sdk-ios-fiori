5df188939b6b87264f892983ebc9b5f1437de503
JunSong <82580343+JunSong-SH@users.noreply.github.com>
2024-12-19 14:14:56 +0800

    Update Sources/FioriSwiftUICore/DataTypes/FioriSlider+DataType.swift
    Co-authored-by: dyongxu <61523257+dyongxu@users.noreply.github.com>

--- Sources/FioriSwiftUICore/DataTypes/FioriSlider+DataType.swift
+++ Sources/FioriSwiftUICore/DataTypes/FioriSlider+DataType.swift
@@ -36,7 +36,7 @@ public extension FioriSlider {
     ///   - showsTrailingAccessory: Indicates whether the trailing accessory view is to be displayed or not. The default value is `true`
     ///   - trailingValueFormat: This optional format is used to format the displayed maximal value of standard slider's range in the trailing accessory view. if provided.
     ///   - onValueChange: An optional callback function is triggered when the user begins to drag the thumb along the standard slider's track to adjust its value. The first boolean property indicates whether the editing process has begun or ended, with `false` signifying that the editing has concluded. The second double property represents the newly adjusted slider value.
-    ///   - thumb: the Shape for  thumb of the standard Slider. By default, it is Circle.
+    ///   - thumb: the shape for thumb of the standard Slider. By default, it is circle.
     ///   - activeTrack: the shape for active track of the standard Slider. By default, it is capsule.
     ///   - inactiveTrack: the shape for inactive track of the standard Slider. By default, it is capsule.
     ///   - thumbHalfWidth: the half width of the thumb of the standard slider. In the context of a circular representation of the thumb, this value is used as the radius. It should be less than 22. The default value is `14`.
