df95ab9a8e700c31e7b354adac8ed99b927f0cc4
Xiaoyu Liu <xiaoyuliu0722@gmail.com>
2025-02-14 16:09:02 +0800

    refactor: 💡 [HCPSDKFIORIUIKIT-2887] signature capture view (#978)

---
+++ Sources/FioriSwiftUICore/_generated/StyleableComponents/StartSignatureAction/StartSignatureActionStyle.generated.swift
@@ -0,0 +1,35 @@
+// Generated using Sourcery 2.1.7 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+import Foundation
+import SwiftUI
+
+public protocol StartSignatureActionStyle: DynamicProperty {
+    associatedtype Body: View
+
+    func makeBody(_ configuration: StartSignatureActionConfiguration) -> Body
+}
+
+struct AnyStartSignatureActionStyle: StartSignatureActionStyle {
+    let content: (StartSignatureActionConfiguration) -> any View
+
+    init(@ViewBuilder _ content: @escaping (StartSignatureActionConfiguration) -> any View) {
+        self.content = content
+    }
+
+    public func makeBody(_ configuration: StartSignatureActionConfiguration) -> some View {
+        self.content(configuration).typeErased
+    }
+}
+
+public struct StartSignatureActionConfiguration {
+    public var componentIdentifier: String = "fiori_startsignatureaction_component"
+    public let startSignatureAction: StartSignatureAction
+
+    public typealias StartSignatureAction = ConfigurationViewWrapper
+}
+
+extension StartSignatureActionConfiguration {
+    func isDirectChild(_ componentIdentifier: String) -> Bool {
+        componentIdentifier == self.componentIdentifier
+    }
+}
