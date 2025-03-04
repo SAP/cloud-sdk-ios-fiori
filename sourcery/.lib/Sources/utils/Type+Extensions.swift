e5d44c4629ff920ec667c7dd6e272b3e4abed6c8
i063052 <jun.song@sap.com>
2025-03-04 14:43:53 +0800

    refactor: 💡 [HCPSDKFIORIUIKIT-2893] SliderPickerItemModel

--- sourcery/.lib/Sources/utils/Type+Extensions.swift
+++ sourcery/.lib/Sources/utils/Type+Extensions.swift
@@ -95,7 +95,8 @@ public extension Type {
                                     "_SortFilterViewModel",
                                     "_SignatureCaptureViewModel",
                                     "_EmptyStateViewModel",
-                                    "_KPIItemModel"]
+                                    "_KPIItemModel",
+                                    "_SliderPickerItemModel"]
         
         if deprecatedComponents.contains(name) {
             return name.replacingOccurrences(of: "Model", with: "")
