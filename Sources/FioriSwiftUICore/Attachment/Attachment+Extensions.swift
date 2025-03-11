b86f45d68b626095df592610a743caeb43b6264f
Charles Xu <charles.xu@sap.com>
2025-03-11 12:46:15 -0700

    fix: 🐛 build warnings part3
    fix build warnings part three.
    
    BREAKING CHANGE: 🧨 0
    
    ✅ Closes: 0

--- Sources/FioriSwiftUICore/Attachment/Attachment+Extensions.swift
+++ Sources/FioriSwiftUICore/Attachment/Attachment+Extensions.swift
@@ -2,11 +2,13 @@
 import SwiftUI
 
 extension View {
+    /// Helper on colloection attachment information, such as title, file size, and last modified date.
     func getFileInfo(fileUrl: URL) -> (URL, String, Int?, Date?)? {
         guard let resourceValues = try? fileUrl.resourceValues(forKeys: [.fileSizeKey, .creationDateKey, .contentModificationDateKey, .nameKey]) else { return nil }
         return (fileUrl, fileUrl.lastPathComponent, resourceValues.fileSize, resourceValues.contentModificationDate)
     }
     
+    /// Helper on formatting file size.
     func format(size: Int) -> String {
         ByteCountFormatter.string(fromByteCount: Int64(size), countStyle: .file)
     }
