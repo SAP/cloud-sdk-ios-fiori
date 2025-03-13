6c49cbd7a40baaf201731164e4fad1704746e33a
hengyi-zhang <hengyizhangcn@gmail.com>
2025-03-13 09:08:04 +0800

    refactor: 💡 [HCPSDKFIORIUIKIT-2882] WelcomeScreen refactor (#1019)
    * refactor: 💡 [HCPSDKFIORIUIKIT-2882] WelcomeScreen refactor
    
    * refactor: 💡 [HCPSDKFIORIUIKIT-2882] WelcomeScreen refactor
    
    * refactor: 💡 [HCPSDKFIORIUIKIT-2882] WelcomeScreen refactor
    
    ---------
    
    Co-authored-by: dyongxu <61523257+dyongxu@users.noreply.github.com>

--- Sources/FioriSwiftUICore/_generated/SupportingFiles/ViewEmptyChecking+Extension.generated.swift
+++ Sources/FioriSwiftUICore/_generated/SupportingFiles/ViewEmptyChecking+Extension.generated.swift
@@ -1071,6 +1071,12 @@ extension TextFieldFormView: _ViewEmptyChecking {
     }
 }
 
+extension TextInput: _ViewEmptyChecking {
+    public var isEmpty: Bool {
+        false
+    }
+}
+
 extension TextInputField: _ViewEmptyChecking {
     public var isEmpty: Bool {
         false
@@ -1247,6 +1253,21 @@ extension ValuePicker: _ViewEmptyChecking {
 extension Watermark: _ViewEmptyChecking {
     public var isEmpty: Bool {
         watermark.isEmpty
+    }
+}
+
+extension WelcomeScreen: _ViewEmptyChecking {
+    public var isEmpty: Bool {
+        title.isEmpty &&
+            description.isEmpty &&
+            icon.isEmpty &&
+            footnote.isEmpty &&
+            action.isEmpty &&
+            secondaryAction.isEmpty &&
+            illustratedMessage.isEmpty &&
+            headlineImage.isEmpty &&
+            legalText.isEmpty &&
+            footerText.isEmpty
     }
 }
 
