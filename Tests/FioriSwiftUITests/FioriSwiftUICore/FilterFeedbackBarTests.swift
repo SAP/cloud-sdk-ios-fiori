79b0db209da578254b997fd5bc5205980d381637
Gu, Jiajun (external - Project) <restaurantt@gmail.com>
2025-03-21 16:28:35 +0800

    refactor: 💡 [HCPSDKFIORIUIKIT-2232]SortFilterView NoteFormView

--- Tests/FioriSwiftUITests/FioriSwiftUICore/FilterFeedbackBarTests.swift
+++ Tests/FioriSwiftUITests/FioriSwiftUICore/FilterFeedbackBarTests.swift
@@ -1,30 +1,27 @@
 @testable import FioriSwiftUICore
 import FioriThemeManager
 import SwiftUI
-import Testing
 import XCTest
 
-struct Test {
-    @Test func defaultUI() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-        
-        #expect(EnvironmentValues().filterFeedbackBarItemBorderWidth == 1)
-        #expect(EnvironmentValues().filterFeedbackBarItemFont == .system(.body))
-        #expect(EnvironmentValues().filterFeedbackBarItemSelectedForegroundColor == DefaultFilterFeedbackBarItemForegroundColor)
-        #expect(EnvironmentValues().filterFeedbackBarItemUnselectedForegroundColor.resolve(in: EnvironmentValues()) == Color.preferredColor(.tertiaryLabel).resolve(in: EnvironmentValues()))
-        #expect(EnvironmentValues().filterFeedbackBarItemSpacing == 6)
-        #expect(EnvironmentValues().filterFeedbackBarItemPadding == 8)
-        #expect(EnvironmentValues().filterFeedbackBarItemMaxWidth == 0)
-        #expect(EnvironmentValues().filterFeedbackBarItemMinHeight == 38)
-        #expect(EnvironmentValues().filterFeedbackBarItemCornerRadius == 10)
-        #expect(EnvironmentValues().filterFeedbackBarItemBackgroundSelectedFillColor.resolve(in: EnvironmentValues()) == Color.clear.resolve(in: EnvironmentValues()))
-        #expect(EnvironmentValues().filterFeedbackBarItemBackgroundUnselectedFillColor.resolve(in: EnvironmentValues()) == Color.preferredColor(.tertiaryFill).resolve(in: EnvironmentValues()))
+final class FilterFeedbackBarTests: XCTestCase {
+    func testDefaultUI() throws {
+        XCTAssertEqual(EnvironmentValues().filterFeedbackBarItemBorderWidth, 1)
+        XCTAssertEqual(EnvironmentValues().filterFeedbackBarItemFont, .system(.body))
+        XCTAssertEqual(EnvironmentValues().filterFeedbackBarItemSelectedForegroundColor, DefaultFilterFeedbackBarItemForegroundColor)
+        XCTAssertEqual(EnvironmentValues().filterFeedbackBarItemUnselectedForegroundColor.resolve(in: EnvironmentValues()), Color.preferredColor(.tertiaryLabel).resolve(in: EnvironmentValues()))
+        XCTAssertEqual(EnvironmentValues().filterFeedbackBarItemSpacing, 6)
+        XCTAssertEqual(EnvironmentValues().filterFeedbackBarItemPadding, 8)
+        XCTAssertEqual(EnvironmentValues().filterFeedbackBarItemMaxWidth, 0)
+        XCTAssertEqual(EnvironmentValues().filterFeedbackBarItemMinHeight, 38)
+        XCTAssertEqual(EnvironmentValues().filterFeedbackBarItemCornerRadius, 10)
+        XCTAssertEqual(EnvironmentValues().filterFeedbackBarItemBackgroundSelectedFillColor.resolve(in: EnvironmentValues()), Color.clear.resolve(in: EnvironmentValues()))
+        XCTAssertEqual(EnvironmentValues().filterFeedbackBarItemBackgroundUnselectedFillColor.resolve(in: EnvironmentValues()), Color.preferredColor(.tertiaryFill).resolve(in: EnvironmentValues()))
         #if !os(visionOS)
-            #expect(EnvironmentValues().filterFeedbackBarItemBackgroundSelectedStrokeColor.resolve(in: EnvironmentValues()) == Color.preferredColor(.tintColor).resolve(in: EnvironmentValues()))
+            XCTAssertEqual(EnvironmentValues().filterFeedbackBarItemBackgroundSelectedStrokeColor.resolve(in: EnvironmentValues()), Color.preferredColor(.tintColor).resolve(in: EnvironmentValues()))
         #else
-            #expect(EnvironmentValues().filterFeedbackBarItemBackgroundSelectedFillColor.resolve(in: EnvironmentValues()) == Color.preferredColor(.primaryLabel).resolve(in: EnvironmentValues()))
+            XCTAssertEqual(EnvironmentValues().filterFeedbackBarItemBackgroundSelectedFillColor.resolve(in: EnvironmentValues()), Color.preferredColor(.primaryLabel).resolve(in: EnvironmentValues()))
         #endif
-        #expect(EnvironmentValues().filterFeedbackBarItemBackgroundUnselectedStrokeColor.resolve(in: EnvironmentValues()) == Color.preferredColor(.separatorOpaque).resolve(in: EnvironmentValues()))
-        #expect(EnvironmentValues().filterFeedbackBarItemBorderWidth == 1)
+        XCTAssertEqual(EnvironmentValues().filterFeedbackBarItemBackgroundUnselectedStrokeColor.resolve(in: EnvironmentValues()), Color.preferredColor(.separatorOpaque).resolve(in: EnvironmentValues()))
+        XCTAssertEqual(EnvironmentValues().filterFeedbackBarItemBorderWidth, 1)
     }
 }
