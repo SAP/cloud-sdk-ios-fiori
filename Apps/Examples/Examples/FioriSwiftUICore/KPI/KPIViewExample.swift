20b7411e64aecaec10c86e83d9e2150041a2f169
zuopengchi <111266960+zuopengchi@users.noreply.github.com>
2025-12-19 09:06:08 +0800

    fix: 🐛 [IOSSDKBUG-1199] [acc] Add subtitles line limit in KPI (#1432)
    Co-authored-by: dyongxu <yong.xu@sap.com>

--- Apps/Examples/Examples/FioriSwiftUICore/KPI/KPIViewExample.swift
+++ Apps/Examples/Examples/FioriSwiftUICore/KPI/KPIViewExample.swift
@@ -20,22 +20,29 @@ public struct KPIExample: View {
     public init() {}
     
     public var body: some View {
-        VStack(spacing: 16) {
-            _KPIItem(data: .components([.unit("$"), .metric("121.98"), .unit("USD")]), subtitle: "Average Spending", action: { print("KPI tapped") })
-                .subtitleModifier { content in
-                    content
-                        .foregroundColor(.yellow)
-                        .font(.caption)
-                }
-            _KPIItem(data: .components([.unit("$"), .metric("999,999.98"), .unit("USD")]), subtitle: "Very Very Very Very Very Very Very Very Very Very Long Subtitle")
-                .disabled(true)
-            _KPIItem(data: .measure(Measurement(value: 200.50, unit: UnitMass.kilograms), self.mf), subtitle: "Current Weight")
-            _KPIItem(data: .percent(0.695), subtitle: "Acceptance Rate")
-                .disabled(true)
-            _KPIItem(data: .duration(92000, self.df), subtitle: "Working Hours")
-            _KPIItem(data: .components([.metric("888,888"), .unit("mi")]), subtitle: "In Store Purchase With a Label That is Two Lines")
-                .disabled(true)
-            _KPIItem(data: .components([.icon(Image(systemName: "heart.fill")), .metric("2K"), .icon(Image(systemName: "hand.thumbsup")), .metric("7.5K")]), subtitle: "Likes & Thumbs-Up")
+        ScrollView {
+            VStack(spacing: 16) {
+                _KPIItem(data: .components([.unit("$"), .metric("121.98"), .unit("USD")]), subtitle: "Average Spending", action: { print("KPI tapped") })
+                    .subtitleModifier { content in
+                        content
+                            .foregroundColor(.yellow)
+                            .font(.caption)
+                    }
+                _KPIItem(data: .components([.unit("$"), .metric("999,999.98"), .unit("USD")]), subtitle: "Very Very Very Very Very Very Very Very Very Very Long Subtitle")
+                    .disabled(true)
+                    .subtitleModifier { content in
+                        content
+                            .lineLimit(nil)
+                    }
+                _KPIItem(data: .measure(Measurement(value: 200.50, unit: UnitMass.kilograms), self.mf), subtitle: "Current Weight")
+                _KPIItem(data: .percent(0.695), subtitle: "Acceptance Rate")
+                    .disabled(true)
+                _KPIItem(data: .duration(92000, self.df), subtitle: "Working Hours")
+                _KPIItem(data: .components([.metric("888,888"), .unit("mi")]), subtitle: "In Store Purchase With a Label That is Two Lines")
+                    .disabled(true)
+                _KPIItem(data: .components([.icon(Image(systemName: "heart.fill")), .metric("2K"), .icon(Image(systemName: "hand.thumbsup")), .metric("7.5K")]), subtitle: "Likes & Thumbs-Up")
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
     }
 }
