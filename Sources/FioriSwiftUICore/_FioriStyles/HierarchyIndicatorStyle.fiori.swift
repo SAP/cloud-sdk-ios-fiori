794c923e3bb24293c666d384bf892e5753b39005
JunSong <82580343+JunSong-SH@users.noreply.github.com>
2026-01-02 14:13:33 +0800

    feat: 🎸 Support for Asynchronous, Multi‑Column Hierarchical View (#1451)
    Co-authored-by: I063052 <I063052@10.207.206.242>
    Co-authored-by: dyongxu <yong.xu@sap.com>

--- Sources/FioriSwiftUICore/_FioriStyles/HierarchyIndicatorStyle.fiori.swift
+++ Sources/FioriSwiftUICore/_FioriStyles/HierarchyIndicatorStyle.fiori.swift
@@ -19,7 +19,7 @@ public struct HierarchyIndicatorBaseStyle: HierarchyIndicatorStyle {
     @Environment(\.sizeCategory) private var sizeCategory
     
     public func makeBody(_ configuration: HierarchyIndicatorConfiguration) -> some View {
-        if configuration.isMultiline || configuration.isLargeSizeCategory(self.sizeCategory) {
+        if configuration.isMultiline || HierarchyIndicatorConfiguration.isLargeSizeCategory(self.sizeCategory) {
             return AnyView(
                 VStack(spacing: HierarchyIndicatorLayout.rowElementSpace) {
                     configuration.icon
@@ -51,17 +51,24 @@ extension HierarchyIndicatorFioriStyle {
     struct ContentFioriStyle: HierarchyIndicatorStyle {
         @Environment(\.sizeCategory) private var sizeCategory
         @Environment(\.onCurrentHierarchyItemChange) private var onCurrentItemChange
+        @Environment(\.hierarchyViewMultiColumnIndex) private var columnIndex
+        @Environment(\.isHierarchyViewMultiColumnLayout) private var isMultiColumnLayout
+        @Environment(\.isHierarchyViewMultiColumnItemActived) private var isItemActived
         @State private var isActive = false
         @State var scale: CGFloat = 1.0
         
         func makeBody(_ configuration: HierarchyIndicatorConfiguration) -> some View {
-            let isLargeSizeCategory = configuration.isLargeSizeCategory(self.sizeCategory)
+            let isLargeSizeCategory = HierarchyIndicatorConfiguration.isLargeSizeCategory(self.sizeCategory)
             let topPadding: CGFloat = (configuration.isMultiline || isLargeSizeCategory) ? 2 : 0
+            var active = configuration.isSelected
+            if let isItemActived, isMultiColumnLayout {
+                active = isItemActived
+            }
             return HierarchyIndicator(configuration)
                 .frame(maxWidth: isLargeSizeCategory ? HierarchyIndicatorLayout.multiLineWideContentWidth : (configuration.isMultiline ? HierarchyIndicatorLayout.multiLineContentWidth : HierarchyIndicatorLayout.singleLineContentWidth), maxHeight: .infinity)
                 .padding(EdgeInsets(top: topPadding, leading: 0, bottom: 0, trailing: 0))
                 .onTapGesture {
-                    if configuration.isClickable, !configuration.isSelected, configuration.onClick != nil {
+                    if configuration.isClickable, !active, configuration.onClick != nil {
                         self.isActive = true // To indicate the indicator was clicked and let icon change to acived icon firstly before follow-up event was triggered
                     
                         // To change the scale for icon to let it like the button when indicator was clicked
@@ -79,7 +86,7 @@ extension HierarchyIndicatorFioriStyle {
                     if !oldValue, newValue, let onClick = configuration.onClick { // 'isActive' to true means the icon was clicked and icon was change. Now, trigger the follow-up event
                         if let itemId = onClick() { // Handle the case when indicator was in HierarchyView or HierarchyItemView
                             DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                                self.onCurrentItemChange?(itemId) // Handle the case when indicator was in HierarchyView only
+                                self.onCurrentItemChange?(itemId, self.columnIndex) // Handle the case when indicator was in HierarchyView only
                             }
                         }
                         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
@@ -109,16 +116,22 @@ extension HierarchyIndicatorFioriStyle {
         func makeBody(_ configuration: TitleConfiguration) -> some View {
             // Not sure why, the modifier, like font, frame and foregroundStyle, don't work on Title(configuration) here.
             // So, use below 'Title' extension to apply all modifier.
-            Title(configuration, backgroundColorScheme: self.backgroundColorScheme, isMultiline: self.hierarchyIndicatorConfiguration.isMultiline, isLargeSizeCategory: self.hierarchyIndicatorConfiguration.isLargeSizeCategory(self.sizeCategory))
+            Title(configuration, backgroundColorScheme: self.backgroundColorScheme, isMultiline: self.hierarchyIndicatorConfiguration.isMultiline, isLargeSizeCategory: HierarchyIndicatorConfiguration.isLargeSizeCategory(self.sizeCategory))
                 .lineLimit(1)
         }
     }
 
     struct IconFioriStyle: IconStyle {
         let hierarchyIndicatorConfiguration: HierarchyIndicatorConfiguration
-    
+        @Environment(\.isHierarchyViewMultiColumnLayout) private var isMultiColumnLayout
+        @Environment(\.isHierarchyViewMultiColumnItemActived) private var isItemActived
+        
         func makeBody(_ configuration: IconConfiguration) -> some View {
-            let icon = configuration.icon.isEmpty ? AnyView(self.hierarchyIndicatorConfiguration.isSelected ? FioriIcon.actions.hierarchyOn : FioriIcon.actions.hierarchyOff) : AnyView(configuration.icon)
+            var active = self.hierarchyIndicatorConfiguration.isSelected
+            if let isItemActived, isMultiColumnLayout {
+                active = isItemActived
+            }
+            let icon = configuration.icon.isEmpty ? AnyView(active ? FioriIcon.actions.hierarchyOn : FioriIcon.actions.hierarchyOff) : AnyView(configuration.icon)
             return Icon(icon: { icon })
                 .font(.fiori(forTextStyle: .callout))
                 .scaledToFill()
