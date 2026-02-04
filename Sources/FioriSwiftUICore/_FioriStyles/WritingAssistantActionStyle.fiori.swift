0bc5334c28d4eabba53cb9d1d67034945cd319c5
Xiaoyu Liu <xiaoyu.liu@sap.com>
2026-02-04 11:11:08 +0800

    fix: 🐛 [1692&1693] enhancement for W.A. disable entry button (#1538)
    * fix: 🐛 [1692&1693] enhancement for W.A. disable entry button
    
    When text is empty, allow to disable the entry button, and also support
    an authorizaiton check for using W.A.
    
    * fix: 🐛 update title for test case to be more clear
    
    ---------
    
    Co-authored-by: dyongxu <dyongxu@gmail.com>

--- Sources/FioriSwiftUICore/_FioriStyles/WritingAssistantActionStyle.fiori.swift
+++ Sources/FioriSwiftUICore/_FioriStyles/WritingAssistantActionStyle.fiori.swift
@@ -16,16 +16,7 @@ public struct WritingAssistantActionFioriStyle: WritingAssistantActionStyle {
     @ViewBuilder
     public func makeBody(_ configuration: WritingAssistantActionConfiguration) -> some View {
         WritingAssistantAction(configuration)
-            .fioriButtonStyle(AIWAButtonStyle())
-    }
-}
-
-struct AIWAButtonStyle: FioriButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        let isPressed = configuration.state == .highlighted || configuration.state == .selected
-        return configuration.label
-            .font(Font.fiori(forTextStyle: .body, weight: .semibold))
-            .foregroundColor(.preferredColor(isPressed ? .tintColorTapState : .tintColor))
+            .fioriButtonStyle(FioriTertiaryButtonStyle())
             .accessibilityElement(children: .combine)
             .accessibilityLabel(NSLocalizedString("AI Writing Assistant", tableName: "FioriSwiftUICore", bundle: Bundle.accessor, comment: "AI Writing Assistant"))
             .accessibilityHint(NSLocalizedString("double tap to open AI Writing Assistant and edit text", tableName: "FioriSwiftUICore", bundle: Bundle.accessor, comment: "double tap to open AI Writing Assistant and edit text"))
