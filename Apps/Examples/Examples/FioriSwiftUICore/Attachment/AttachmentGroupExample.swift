99eb809d582dd88a8746401a15b96664b4166679
David Xu <61523257+dyongxu@users.noreply.github.com>
2026-09-02 19:18:52 -0700

    ci: 🎡 upgrade to Xcode 27.0 beta

--- Apps/Examples/Examples/FioriSwiftUICore/Attachment/AttachmentGroupExample.swift
+++ Apps/Examples/Examples/FioriSwiftUICore/Attachment/AttachmentGroupExample.swift
@@ -1,8 +1,3 @@
-🌱 Cloning SwiftFormat 0.53.4
-🌱 Resolving package
-🌱 Building product swiftformat
-🌱 Installed SwiftFormat 0.53.4
-🌱 Running swiftformat 0.53.4...
 import FioriSwiftUICore
 import FioriThemeManager
 import OSLog
