bdc7bf25be4dfb5be0cf36a84a493dd82f32654f
hengyi-zhang <hengyi.zhang@sap.com>
2026-09-14 14:30:48 +0800

    fix: 🐛 [IOSSDKBUG-2276] Liquid Glass Updates for Multi-message Handling (#1765) (#1778)
    * fix: 🐛 [IOSSDKBUG-2276] Liquid Glass Updates for Multi-message Handling (#1765)
    
    * fix: 🐛 [IOSSDKBUG-2276] Adjust header layout for multi-message banner sheet
    
    * fix: 🐛 [IOSSDKBUG-2276] Liquid Glass Updates for Multi-message Handling
    
    * fix: 🐛 [IOSSDKBUG-2276] Use NavigationStack in BannerMultiMessageSheet
    
    * fix: 🐛 [IOSSDKBUG-2276] Adapt BannerMultiMessageSheet in visionOS (#1782)

--- Apps/Examples/Examples/FioriSwiftUICore/ProfileHeader/ProfileHeaderStaticExample.swift
+++ Apps/Examples/Examples/FioriSwiftUICore/ProfileHeader/ProfileHeaderStaticExample.swift
@@ -57,7 +57,10 @@ struct ProfileHeaderStaticExample: View {
     
     var body: some View {
         if #available(iOS 26, *) {
-            list.scrollEdgeEffectStyle(.hard, for: .top)
+            list
+            #if !os(visionOS)
+            .scrollEdgeEffectStyle(.hard, for: .top)
+            #endif
         } else {
             self.list
         }
