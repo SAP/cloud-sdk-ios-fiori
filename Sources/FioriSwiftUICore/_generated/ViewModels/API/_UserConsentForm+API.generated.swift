93ff44b5af99064f055193d317c0cd6b9f0bcbd2
Xiaoyu Liu <xiaoyu.liu@sap.com>
2026-09-22 21:41:39 +0800

    fix: 🐛 tuple state is blocked in Xcode 27 (#1803)

--- Sources/FioriSwiftUICore/_generated/ViewModels/API/_UserConsentForm+API.generated.swift
+++ Sources/FioriSwiftUICore/_generated/ViewModels/API/_UserConsentForm+API.generated.swift
@@ -24,7 +24,8 @@ public struct _UserConsentForm<NextActionView: View, CancelActionView: View, All
 	let _didDeny: ((Bool) -> Void)?
 	let _didCancel: (() -> Void)?
 	@State var _pageIndex = 0
-	@State var _showAlert: (Bool, UserConsentAlertType) = (false, .deny)
+	@State var _showAlert = false
+	@State var _alertType: UserConsentAlertType = .deny
 
     private var isModelInit: Bool = false
 	private var isNextActionNil: Bool = false
